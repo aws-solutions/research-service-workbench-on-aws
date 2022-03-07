05d94d7bdff1a2671244de88642fc6f2ee7244ab
Samir Sharan <issharan@amazon.com>
2022-03-07 14:25:08 -0500

    test ci update

--- solutions/swb-app/src/app.ts
+++ solutions/swb-app/src/app.ts
@@ -9,6 +9,7 @@ export type AppOptions = {
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
   // Place here your custom code!
   // Do not touch the following lines
+
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
@@ -29,3 +30,5 @@ const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void>
 
 export default app;
 export { app };
+
+console.log('Hi');
