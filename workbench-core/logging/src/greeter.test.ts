7f6350751ad7258559c0bf116aef6880f0a4a1e8
Brendon Hudnell <bhudnell@amazon.com>
2022-03-29 16:24:50 -0600

    feat: initialize logging package

---
+++ workbench-core/logging/src/greeter.test.ts
@@ -0,0 +1,15 @@
+import { Greeter } from './greeter';
+
+const g = new Greeter();
+const name = 'Bob';
+describe('Hello tests', () => {
+  it('test', () => {
+    expect(g.sayHello(name)).toStrictEqual('Hello, Bob');
+  });
+});
+
+describe('Goodbye tests', () => {
+  it('test', () => {
+    expect(g.sayGoodbye(name)).toStrictEqual('Goodbye, Bob');
+  });
+});
