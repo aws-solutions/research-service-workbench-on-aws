ad4a308cb8589926a384ac98422fb0705ef85e1a
Brendon Hudnell <bhudnell@amazon.com>
2022-04-01 15:21:27 -0600

    feat: completed unit tests

--- workbench-core/logging/src/consoleTransport.test.ts
+++ workbench-core/logging/src/consoleTransport.test.ts
@@ -1,27 +1,68 @@
 import { makeLogger } from '.';
 
-describe('To console tests', () => {
-  it('should default to info log level when there is no parameter specified or environment variable defined', () => {
+/**
+ * Log levels (highest to lowest priority):
+ *
+ * error: 0,
+ * warn: 1,
+ * info: 2,
+ * http: 3,
+ * verbose: 4,
+ * debug: 5,
+ * silly: 6
+ *
+ */
+
+describe('ConsoleTransport tests', () => {
+  it('error log level should output using console.error', () => {
     const logger = makeLogger({ logLevel: 'silly' });
 
     const errorSpy = jest.spyOn(console, 'error');
-    const warnSpy = jest.spyOn(console, 'warn');
-    const infoSpy = jest.spyOn(console, 'info');
-    const debugSpy = jest.spyOn(console, 'debug');
-    const logSpy = jest.spyOn(console, 'log');
 
-    logger.error('this should be logged');
-    logger.warn('this should be logged');
-    logger.info('this should be logged');
-    logger.http('this shouldnt be logged');
-    logger.verbose('this shouldnt be logged');
-    logger.debug('this shouldnt be logged');
-    logger.silly('this shouldnt be logged');
+    logger.error('a log message');
 
     expect(errorSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('warn log level should output using console.warn', () => {
+    const logger = makeLogger({ logLevel: 'silly' });
+
+    const warnSpy = jest.spyOn(console, 'warn');
+
+    logger.warn('this should be logged');
+
     expect(warnSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('info log level should output using console.info', () => {
+    const logger = makeLogger({ logLevel: 'silly' });
+
+    const infoSpy = jest.spyOn(console, 'info');
+
+    logger.info('this should be logged');
+
     expect(infoSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('debug log level should output using console.debug', () => {
+    const logger = makeLogger({ logLevel: 'silly' });
+
+    const debugSpy = jest.spyOn(console, 'debug');
+
+    logger.debug('this shouldnt be logged');
+
     expect(debugSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('http, verbose, and silly log level should output using console.log', () => {
+    const logger = makeLogger({ logLevel: 'silly' });
+
+    const logSpy = jest.spyOn(console, 'log');
+
+    logger.http('this shouldnt be logged');
+    logger.verbose('this shouldnt be logged');
+    logger.silly('this shouldnt be logged');
+
     expect(logSpy).toHaveBeenCalledTimes(3);
   });
 });
---
+++ workbench-core/logging/src/logger.test.ts
@@ -0,0 +1,37 @@
+import { makeLogger } from '.';
+
+describe('makeLogger tests', () => {
+  it('log level should default to info when there is no parameter or environment variable defined', () => {
+    const logger = makeLogger();
+
+    expect(logger.level).toBe('info');
+  });
+
+  it('log level should equal process.env.LOG_LEVEL when there is no parameter included', () => {
+    process.env.LOG_LEVEL = 'error';
+    const logger = makeLogger();
+
+    expect(logger.level).toBe('error');
+  });
+
+  it('log level should equal options.logLevel when parameter is included', () => {
+    const logger = makeLogger({ logLevel: 'debug' });
+
+    expect(logger.level).toBe('debug');
+  });
+
+  it('metadata should be undefined when parameter is not included', () => {
+    const logger = makeLogger();
+
+    expect(logger.defaultMeta).toMatchObject({ meta: undefined });
+  });
+
+  it('metadata should be defined when parameter is included', () => {
+    const metadata = {
+      some: 'metadata'
+    };
+    const logger = makeLogger({ metadata });
+
+    expect(logger.defaultMeta).toMatchObject({ meta: metadata });
+  });
+});
