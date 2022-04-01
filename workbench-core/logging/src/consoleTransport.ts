340f393a6768af1905b15965ee56b2eea01f7427
Brendon Hudnell <bhudnell@amazon.com>
2022-04-01 17:15:07 -0600

    fix: fixing semgrep-sast errors

--- workbench-core/logging/src/consoleTransport.ts
+++ workbench-core/logging/src/consoleTransport.ts
@@ -1,30 +1,40 @@
 import Transport from 'winston-transport';
+import { LEVEL, MESSAGE } from 'triple-beam';
+
+/**
+ * The info passed from winston to the transport.
+ */
+interface LoggerInfo {
+  [MESSAGE]: string;
+  [LEVEL]: string;
+}
 
 /**
  * A direct to console transport for use by the Winston logger.
+ *
+ * This transport assumes that the winston logger has the format.json() formatter as the finalizing formatter.
  */
 export class ConsoleTransport extends Transport {
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
-  public log(info: any, callback: () => void): void {
+  public log(info: LoggerInfo, callback: () => void): void {
     /* istanbul ignore next */
     setImmediate(() => this.emit('logged', info));
 
     // Use console here so request ID and log level can be automatically attached in CloudWatch log
-    switch (info[Symbol.for('level')]) {
+    switch (info[LEVEL]) {
       case 'debug':
-        console.debug(info[Symbol.for('message')]);
+        console.debug(info[MESSAGE]);
         break;
       case 'info':
-        console.info(info[Symbol.for('message')]);
+        console.info(info[MESSAGE]);
         break;
       case 'warn':
-        console.warn(info[Symbol.for('message')]);
+        console.warn(info[MESSAGE]);
         break;
       case 'error':
-        console.error(info[Symbol.for('message')]);
+        console.error(info[MESSAGE]);
         break;
       default:
-        console.log(info[Symbol.for('message')]);
+        console.log(info[MESSAGE]);
         break;
     }
 
