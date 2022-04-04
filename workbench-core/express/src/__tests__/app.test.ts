51b56d51a859f20c6f5f814d24fe0293b7b8892e
Samir Sharan <sharan.samir@gmail.com>
2022-04-04 04:31:59 -0400

    chore: code deploy

--- workbench-core/express/src/__tests__/app.test.ts
+++ workbench-core/express/src/__tests__/app.test.ts
@@ -1,42 +1,37 @@
-// import axios from 'axios';
-
-// describe('Hello World', () => {
-//   test("Service returns 'Hello World'", async () => {
-//     try {
-//       console.log(process.env.SERVICE_ENDPOINT);
-//       const SERVICE_ENDPOINT: string =
-//         process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
-//       const response = await axios.get(SERVICE_ENDPOINT, {
-//         proxy: {
-//           host: 'localhost',
-//           port: 8080
-//         }
-//       });
-//       expect(response.data).toBe('Hello World');
-//     } catch (e) {
-//       console.error(e);
-//       throw e;
-//     }
-//   });
-// });
-
-import request from 'request';
+import axios from 'axios';
 
 describe('Hello World', () => {
   test("Service returns 'Hello World'", async () => {
-    const SERVICE_ENDPOINT: string =
-      process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
-    request({
-      uri: SERVICE_ENDPOINT,
-      method: 'GET'
-    })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .on('response', function (response: any) {
-        expect(response.statusCode).toBe(200);
-      })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .on('error', function (error: any) {
-        console.error(error.message);
-      });
+    try {
+      console.log(process.env.SERVICE_ENDPOINT);
+      const SERVICE_ENDPOINT: string =
+        process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
+      const response = await axios.get(SERVICE_ENDPOINT, {});
+      expect(response.data).toBe('Hello World');
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
   });
 });
+
+// import request from 'request';
+
+// describe('Hello World', () => {
+//   test("Service returns 'Hello World'", async () => {
+//     const SERVICE_ENDPOINT: string =
+//       process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
+//     request({
+//       uri: SERVICE_ENDPOINT,
+//       method: 'GET'
+//     })
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       .on('response', function (response: any) {
+//         expect(response.statusCode).toBe(200);
+//       })
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       .on('error', function (error: any) {
+//         console.error(error.message);
+//       });
+//   });
+// });
