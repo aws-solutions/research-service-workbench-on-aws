941ab181fdb78a1c2df2f6da37c21e49bb1664e4
Samir Sharan <issharan@amazon.com>
2022-04-15 12:50:55 -0400

    chore: cdk nag

--- workbench-core/infrastructure/src/index.ts
+++ workbench-core/infrastructure/src/index.ts
@@ -2,8 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from './infrastructure-stack';
-import { App } from 'aws-cdk-lib';
+import { App, Aspects } from 'aws-cdk-lib';
+// import { AwsSolutionsChecks } from 'cdk-nag';
 
 const app: App = new cdk.App();
 // eslint-disable-next-line no-new
 new InfrastructureStack(app, 'InfrastructureStack');
+
+// Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}));
--- workbench-core/infrastructure/src/infrastructure-stack.ts
+++ workbench-core/infrastructure/src/infrastructure-stack.ts
@@ -1,19 +1,41 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, aws_logs } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { CloudWatchLogGroup } from 'aws-cdk-lib/aws-events-targets';
+import { CfnStage } from 'aws-cdk-lib/aws-apigatewayv2';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class InfrastructureStack extends Stack {
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      retention: RetentionDays.ONE_WEEK,
+      logGroupName: 'HttpApiLogGroup'
+    });
+
+    const lambdaRole = new Role(this, 'LambdaRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com')
+    });
+
+    lambdaRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+        resources: [logGroup.logGroupArn]
+      })
+    );
+
     const lambdaService = new Function(this, 'LambdaService', {
       runtime: Runtime.NODEJS_14_X,
       handler: 'index.handler',
       code: Code.fromAsset('../infrastructure/lambda-build/archive.zip'),
-      functionName: 'LambdaService'
+      functionName: 'LambdaService',
+      role: lambdaRole
     });
 
     const httpLambdaIntegration = new HttpLambdaIntegration('HttpLambda', lambdaService);
@@ -22,6 +44,19 @@ export class InfrastructureStack extends Stack {
       defaultIntegration: httpLambdaIntegration,
       apiName: 'HttpApiService'
     });
+
+    // const httpApiStage = new HttpStage(this, 'HttpStage', {
+    //   httpApi,
+    //   autoDeploy: true,
+    //   stageName: "$default"
+    // })
+
+    const defaultStage = httpApi.defaultStage?.node.defaultChild as CfnStage;
+
+    defaultStage.accessLogSettings = {
+      destinationArn: logGroup.logGroupArn,
+      format: `$context.identity.sourceIp - - [$context.requestTime] "$context.httpMethod $context.routeKey $context.protocol" $context.status $context.responseLength $context.requestId`
+    };
 
     // eslint-disable-next-line no-new
     new CfnOutput(this, 'HttpEndpoint', {
