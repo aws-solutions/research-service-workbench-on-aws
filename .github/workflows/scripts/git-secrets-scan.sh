fd8919dfda5bf9227f7564750c91248780b7cee1
Samir Sharan <issharan@amazon.com>
2022-04-17 16:04:54 -0400

    chore: ci update

--- .github/workflows/scripts/git-secrets-scan.sh
+++ .github/workflows/scripts/git-secrets-scan.sh
@@ -2,45 +2,39 @@
 
 # set -euo pipefail
 
-ci_flag=''
-return_code=0
+staged_flag=''
 
 if [ $# -gt 0 ] && [ $1 ]; then
-    ci_flag='true'
+    staged_flag='true'
 fi
 
+echo 'Git-secrets artifact path not set. Using ./.tools'
 mkdir -p .tools && {
     if [[ ! -d .tools/git-secrets ]] ; then
         echo "======================================================================"
         echo "Downloading git-secrets"
-        cd .tools 
-        git clone https://github.com/awslabs/git-secrets.git && cd git-secrets
-        make install
-        git secrets --register-aws --global
-        # Prevent leakage of internal tools
-        git secrets --add '[aA]pollo|[bB]razil|[cC]oral|[oO]din' --global
-        git secrets --add 'tt\.amazon\.com|issues\.amazon\.com|cr\.amazon\.com' --global
-        # Prevent leakage of aws-iso
-        git secrets --add 'ic\.gov|sgov\.gov' --global
-        git secrets --add 'us-iso|aws-iso' --global
-        git secrets --add 'smil\.mil' --global
+        (cd .tools && git clone --depth 1 https://github.com/awslabs/git-secrets.git)
     fi
 }
 export GIT_SECRETS_DIR=./.tools/git-secrets
+
 export PATH=$PATH:${GIT_SECRETS_DIR}
+${GIT_SECRETS_DIR}/git-secrets --register-aws
 
 # Run git-secrets only on staged files
-echo "Running git-secrets-scan"
-return_code=`${GIT_SECRETS_DIR}/git-secrets --scan`
+if [ $staged_flag ]; then
+    echo "Running git-secrets-scan on staged files"
+    ${GIT_SECRETS_DIR}/git-secrets --scan --cached
+else
+    return_code=`${GIT_SECRETS_DIR}/git-secrets --scan`
+fi
 
 if [ -d .tools/git-secrets ]; then
     rm -rf .tools/git-secrets && echo ".tools/git-secrets deleted !"
 fi
 
-if [ $return_code ];
-then
-    echo "git-secrets scan ok"
+if [[ $return_code ]]; then
+    echo "git-secrets scan detected secrets"
 else
-    echo "Secrets detected please check the logs"
-    exit $return_code
+    echo "git-secrets scan ok !"
 fi
