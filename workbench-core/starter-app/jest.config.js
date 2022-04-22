c64aabd5a5a88e668eacf98d87ec759e0304fb42
Miguel Abreu <miabreu@amazon.com>
2022-04-22 10:21:33 -0400

    fix: build pipeline

--- workbench-core/starter-app/jest.config.js
+++ workbench-core/starter-app/jest.config.js
@@ -1,24 +1,3 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/context/(.*)$': '<rootDir>/context/$1',
-    '^@/models/(.*)$': '<rootDir>/models/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/styles/(.*)$': '<rootDir>/styles/$1'
-  },
-  testEnvironment: 'jest-environment-jsdom'
-};
-
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = {
   collectCoverage: true,
@@ -33,6 +12,6 @@ module.exports = {
     }
   },
   coverageReporters: ['json-summary', 'json'],
-
-  ...createJestConfig(customJestConfig)
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom'
 };
