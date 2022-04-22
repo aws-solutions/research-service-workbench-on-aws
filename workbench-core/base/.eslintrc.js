4d6c24c6a129ba3b83b781e2ce708a8129ee0506
Samir Sharan <sharan.samir@gmail.com>
2022-04-22 19:32:39 -0400

    chore: gitlab to github migration (#64)
    * chore: gitlab to github migration
    
    Co-authored-by: Samir Sharan <issharan@amazon.com>

--- workbench-core/base/.eslintrc.js
+++ workbench-core/base/.eslintrc.js
@@ -1,6 +1,7 @@
-require('@amzn/eslint-config-workbench-core-eslint-custom/node_modules/@rushstack/eslint-config/patch/modern-module-resolution');
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@amzn/workbench-core-eslint-custom'],
+  extends: ['@amzn/eslint-config-workbench-core-eslint-custom'],
   parserOptions: { tsconfigRootDir: __dirname }
 };
