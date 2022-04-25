d8d7eb0bb54c6ccfe37591b59420fcda0b925f47
HaiTao Zhang <htzhang@amazon.com>
2022-04-25 13:58:07 -0700

    refactor(authorizationplugin): name change permissionsRequired to requiredPermissions

--- workbench-core/authorization/src/authorizationPlugin.ts
+++ workbench-core/authorization/src/authorizationPlugin.ts
@@ -8,9 +8,9 @@ export default interface AuthorizationPlugin {
   /**
    * Checks whether a {@link User} is authorized given a set of {@link Permission}.
    * @param user - {@link User}.
-   * @param permissionsRequired - An array of {@link Permission}.
+   * @param requiredPermissions - An array of {@link Permission}.
    *
    * @returns A promise for a boolean stating whether or not they are authorized.
    */
-  isAuthorized(user: User, permissionsRequired: Permission[]): Promise<boolean>;
+  isAuthorized(user: User, requiredPermissions: Permission[]): Promise<boolean>;
 }
