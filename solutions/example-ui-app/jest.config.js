c6319355a97f960fc23366cdf7a0c2ddb44cf083
Miguel Abreu <miabreu@amazon.com>
2022-04-25 14:33:59 -0400

    chore: bump test coverage thresholds

--- solutions/example-ui-app/jest.config.js
+++ solutions/example-ui-app/jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   coverageDirectory: 'temp/coverage',
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
     }
   },
   coverageReporters: ['json-summary', 'json'],
