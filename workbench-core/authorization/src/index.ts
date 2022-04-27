82f14a24db219c7329b0c0d1cca56ecd6e6fa5bf
HaiTao Zhang <haitaozhang246@gmail.com>
2022-04-27 13:41:41 -0700

    feat: Base interfaces for Authorization (#67)
    * feat: create authPlugin and permPlugins bare bone structure, needs to be refined more
    
    * feat: base interfaces completed, comments added
    
    * chore: merge develop (#66)
    
    * feat: env lifecycle service
    
    * fix: IAM policy SID can't have dashes
    
    * chore: gitlab to github migration (#64)
    
    * chore: gitlab to github migration
    
    Co-authored-by: Samir Sharan <issharan@amazon.com>
    
    * chore: merge develop
    
    Co-authored-by: Sanket Dharwadkar <sdharwad@amazon.com>
    Co-authored-by: Samir Sharan <issharan@amazon.com>
    
    * feat: add index.ts
    
    * chore: fix merge conflict
    
    * refactor(authorizationplugin): name change permissionsRequired to requiredPermissions
    
    * refactor: removed requiredPermissions and abstract using Operation. Additional tsdoc
    
    * chore: fix merge conflict
    
    * refactor(permissionsplugin): change inverted to effect in PermissionsPlugin
    
    Co-authored-by: HaiTao Zhang <htzhang@amazon.com>
    Co-authored-by: Samir Sharan <sharan.samir@gmail.com>
    Co-authored-by: Sanket Dharwadkar <sdharwad@amazon.com>
    Co-authored-by: Samir Sharan <issharan@amazon.com>

--- workbench-core/authorization/src/index.ts
+++ workbench-core/authorization/src/index.ts
@@ -1,1 +1,3 @@
-export { Greeter } from './greeter';
+export { default as PermissionsPlugin, PermissionsMap, Permission, Operation } from './permissionsPlugin';
+export { default as AuthorizationPlugin } from './authorizationPlugin';
+export { Action } from './action';
