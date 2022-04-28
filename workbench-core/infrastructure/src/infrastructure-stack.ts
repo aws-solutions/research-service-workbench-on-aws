f2cd8c7d28bfdd01c57c1a99f41dccf3f5cb6888
Samir Sharan <issharan@amazon.com>
2022-04-28 16:46:12 -0400

    chore: allow lambda to write to custom log group

--- workbench-core/infrastructure/src/infrastructure-stack.ts
+++ workbench-core/infrastructure/src/infrastructure-stack.ts
@@ -4,7 +4,7 @@ import { Construct } from 'constructs';
 import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class InfrastructureStack extends Stack {
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
@@ -16,24 +16,33 @@ export class InfrastructureStack extends Stack {
       logGroupName: 'HttpApiLogGroup'
     });
 
-    const lambdaRole = new Role(this, 'LambdaRole', {
-      assumedBy: new ServicePrincipal('lambda.amazonaws.com')
+    const lambdaPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+      resources: [logGroup.logGroupArn],
+      principals: [new ServicePrincipal('lambda.amazonaws.com')]
     });
 
-    lambdaRole.addToPolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
-        resources: [logGroup.logGroupArn]
-      })
-    );
+    logGroup.addToResourcePolicy(lambdaPolicy);
+
+    // const lambdaRole = new Role(this, 'LambdaRole', {
+    //   assumedBy: new ServicePrincipal('lambda.amazonaws.com')
+    // });
+
+    // lambdaRole.addToPolicy(
+    //   new PolicyStatement({
+    //     effect: Effect.ALLOW,
+    //     actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+    //     resources: [logGroup.logGroupArn]
+    //   })
+    // );
 
     const lambdaService = new Function(this, 'LambdaService', {
       runtime: Runtime.NODEJS_14_X,
       handler: 'index.handler',
       code: Code.fromAsset('../infrastructure/lambda-build/archive.zip'),
-      functionName: 'LambdaService',
-      role: lambdaRole
+      functionName: 'LambdaService'
+      // role: lambdaRole
     });
 
     const httpLambdaIntegration = new HttpLambdaIntegration('HttpLambda', lambdaService);
