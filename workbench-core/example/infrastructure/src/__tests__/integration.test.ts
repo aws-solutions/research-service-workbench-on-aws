aec1654b96366550475340dbdb45a4f0aecc097d
Samir Sharan <issharan@amazon.com>
2022-04-29 17:00:35 -0400

    chore: add integration test

---
+++ workbench-core/example/infrastructure/src/__tests__/integration.test.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+describe('Hello World', () => {
+  test("Service returns 'Hello World'", async () => {
+    try {
+      console.log(process.env.SERVICE_ENDPOINT);
+      const SERVICE_ENDPOINT: string =
+        process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
+      const response = await axios.get(SERVICE_ENDPOINT, {});
+      expect(response.data).toBe('Hello World');
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  });
+});
