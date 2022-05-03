af53ae910f4997fd4de92f7442c67830919a5a94
Samir Sharan <insignias@users.noreply.github.com>
2022-05-03 00:01:23 -0400

    style: rename eslint custom package to @amzn/workbench-core-eslint-custom

--- solutions/example-ui-app/.eslintrc.js
+++ solutions/example-ui-app/.eslintrc.js
@@ -3,7 +3,7 @@
 require('@amzn/eslint-config-workbench-core-eslint-custom/node_modules/@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@amzn/eslint-config-workbench-core-eslint-custom', 'next/core-web-vitals'],
+  extends: ['@amzn/workbench-core-eslint-custom', 'next/core-web-vitals'],
   parserOptions: { tsconfigRootDir: __dirname },
   plugins: ['testing-library'],
   overrides: [
