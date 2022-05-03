02698416ae96e5d92762e883cd4e38d81b93faac
Samir Sharan <insignias@users.noreply.github.com>
2022-05-03 18:56:55 -0400

    chore: minor updates

--- workbench-core/example/infrastructure/src/__tests__/integration.test.ts
+++ workbench-core/example/infrastructure/src/__tests__/integration.test.ts
@@ -6,8 +6,7 @@ describe('IntegrationTest', () => {
       if (process.env.SERVICE_ENDPOINT) {
         console.log(process.env.SERVICE_ENDPOINT);
       }
-      const SERVICE_ENDPOINT: string =
-        process.env.SERVICE_ENDPOINT || 'https://l51vbx69s4.execute-api.us-west-2.amazonaws.com/';
+      const SERVICE_ENDPOINT: string = process.env.SERVICE_ENDPOINT || 'dummy-api.com';
       const response = await axios.get(SERVICE_ENDPOINT, {});
       expect(response.data).toBe('Hello World');
     } catch (e) {
