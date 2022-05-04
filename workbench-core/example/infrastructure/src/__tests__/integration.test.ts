1611ac86af1f3200ad881086e961bf49d6ed8e46
Samir Sharan <insignias@users.noreply.github.com>
2022-05-04 18:46:39 -0400

    chore: integration Test update

--- workbench-core/example/infrastructure/src/__tests__/integration.test.ts
+++ workbench-core/example/infrastructure/src/__tests__/integration.test.ts
@@ -6,10 +6,10 @@ describe('IntegrationTest', () => {
       if (process.env.SERVICE_ENDPOINT) {
         console.log(process.env.SERVICE_ENDPOINT);
       }
-      const SERVICE_ENDPOINT: string = process.env.SERVICE_ENDPOINT || 'xxx';
+      const SERVICE_ENDPOINT: string = process.env.SERVICE_ENDPOINT!;
       const response = await axios.get(SERVICE_ENDPOINT, {
         headers: {
-          Authorization: process.env.TEST_APIGATEWAY_ID_TOKEN
+          Authorization: process.env.TEST_APIGATEWAY_ID_TOKEN!
         }
       });
       expect(response.data).toBe('Hello World');
