22e108a655a042ea3055e9b9a3a19a5fd4455932
Samir Sharan <insignias@users.noreply.github.com>
2022-05-04 16:47:08 -0400

    feat: apiGateway authorizer

--- workbench-core/example/infrastructure/src/infrastructure-stack.ts
+++ workbench-core/example/infrastructure/src/infrastructure-stack.ts
@@ -1,9 +1,17 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+/* eslint-disable no-new */
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpUserPoolAuthorizer } from '@aws-cdk/aws-apigatewayv2-authorizers-alpha';
 import { join } from 'path';
+import {
+  AccountRecovery,
+  UserPool,
+  UserPoolClient,
+  UserPoolClientIdentityProvider
+} from 'aws-cdk-lib/aws-cognito';
 
 export class InfrastructureStack extends Stack {
   public constructor(scope: Construct, id: string, props?: StackProps) {
@@ -18,12 +26,61 @@ export class InfrastructureStack extends Stack {
 
     const httpLambdaIntegration = new HttpLambdaIntegration('HttpLambda', lambdaService);
 
+    const userPool = new UserPool(this, 'UserPool', {
+      userPoolName: `my-user-pool`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      selfSignUpEnabled: true,
+      signInAliases: {
+        username: true,
+        email: true
+      },
+      autoVerify: {
+        email: true
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: false,
+        requireDigits: false,
+        requireUppercase: false,
+        requireSymbols: false
+      },
+      accountRecovery: AccountRecovery.EMAIL_ONLY
+    });
+
+    const userPoolClient = new UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+      authFlows: {
+        adminUserPassword: true,
+        userPassword: true,
+        custom: true,
+        userSrp: true
+      },
+      supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO]
+    });
+
+    const authorizer = new HttpUserPoolAuthorizer('user-pool-authorizer', userPool, {
+      userPoolClients: [userPoolClient],
+      identitySource: ['$request.header.Authorization']
+    });
+
     const httpApi = new HttpApi(this, 'HttpApi', {
       defaultIntegration: httpLambdaIntegration,
+      defaultAuthorizer: authorizer,
       apiName: 'HttpApiService'
     });
 
-    // eslint-disable-next-line no-new
+    new CfnOutput(this, 'region', {
+      value: Stack.of(this).region
+    });
+
+    new CfnOutput(this, 'userPoolId', {
+      value: userPool.userPoolId
+    });
+
+    new CfnOutput(this, 'userPoolClientId', {
+      value: userPoolClient.userPoolClientId
+    });
+
     new CfnOutput(this, 'HttpEndpoint', {
       value: httpApi.apiEndpoint,
       exportName: 'HttpApiEndPoint'
