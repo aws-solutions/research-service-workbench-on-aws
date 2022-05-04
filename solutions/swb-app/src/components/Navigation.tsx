5c9dd3e08e896b37c030f5702e05d55a610b44c6
Clara <welclara@amazon.com>
2022-05-04 10:04:11 -0500

    fix: fix nav text, remove unneeded consts

--- solutions/swb-app/src/components/Navigation.tsx
+++ solutions/swb-app/src/components/Navigation.tsx
@@ -15,46 +15,29 @@ export default function Navigation({
   onFollowHandler
 }: NavigationProps): JSX.Element {
   const { t } = useTranslation();
-  // Left nav for admin
-  const adminNavHeader: SideNavigationProps.Header = {
-    text: t('Navigation.Administrator'),
-    href: '#/admin'
-  };
   const adminNavItems: ReadonlyArray<SideNavigationProps.Item> = [
     {
       type: 'section',
-      text: t('Navigation.Dashboards'),
+      text: t('Dashboards'),
       items: [
-        { type: 'link', text: t('Navigation.ManageDashboards'), href: '#/admin/dashboards' },
-        { type: 'link', text: t('Navigation.CreateDashboard'), href: '#/admin/dashboard/create' },
-        { type: 'link', text: t('Navigation.ViewPublishedDashboards'), href: '#/' }
+        { type: 'link', text: t('Manage Dashboards'), href: '#/dashboards/manage' },
+        { type: 'link', text: t('All Dashboards'), href: '#/dashboards' },
+        { type: 'link', text: t('Project Dashboards'), href: '#/dashboards/project' }
       ]
     },
-    {
-      type: 'section',
-      text: t('Navigation.Users'),
-      items: [
-        { type: 'link', text: t('Navigation.ManageUsers'), href: '#/admin/users' },
-        { type: 'link', text: t('Navigation.InviteUsers'), href: '#/admin/users/invite' }
-      ]
-    },
-    { type: 'link', text: t('Navigation.Workspaces'), href: '#/workspaces' },
-    { type: 'link', text: t('Navigation.Datasets'), href: '#/datasets' }
+    { type: 'link', text: t('Users'), href: '#/users' },
+    { type: 'link', text: t('Workspaces'), href: '#/workspaces' },
+    { type: 'link', text: t('Datasets'), href: '#/datasets' }
   ];
-  // Left nav for users
-  const userNavHeader: SideNavigationProps.Header = {
-    text: t('Navigation.User'),
-    href: '#/user'
-  };
   const userNavItems: ReadonlyArray<SideNavigationProps.Item> = [
-    { type: 'link', text: t('Navigation.Workspaces'), href: '#/workspaces' },
-    { type: 'link', text: t('Navigation.Datasets'), href: '#/datasets' },
+    { type: 'link', text: t('Workspaces'), href: '#/workspaces' },
+    { type: 'link', text: t('Datasets'), href: '#/datasets' },
     {
       type: 'section',
-      text: t('Navigation.Dashboard'),
+      text: t('Dashboards'),
       items: [
-        { type: 'link', text: t('Navigation.AllDashboards'), href: '#/dashboard/all' },
-        { type: 'link', text: t('Navigation.ProjectDashboards'), href: '#/dashvoard/projects' }
+        { type: 'link', text: t('All Dashboards'), href: '#/dashboards' },
+        { type: 'link', text: t('Project Dashboards'), href: '#/dashboards/projects' }
       ]
     }
   ];
@@ -66,7 +49,6 @@ export default function Navigation({
     // TODO: add logic for which nav to display (admin or user)
     <SideNavigation
       activeHref={activeHref}
-      header={header ? header : userNavHeader}
       items={items ? items : userNavItems}
       onFollow={onFollowHandler ? onFollowHandler : defaultOnFollowHandler}
     />
