2e359cd5a1027957b8232c51fec808ff7aa7c422
Samir Sharan <insignias@users.noreply.github.com>
2022-05-04 18:43:41 -0400

    chore: apiGateway authorizer

--- workbench-core/example/infrastructure/src/__tests__/integration.test.ts
+++ workbench-core/example/infrastructure/src/__tests__/integration.test.ts
@@ -7,7 +7,11 @@ describe('IntegrationTest', () => {
         console.log(process.env.SERVICE_ENDPOINT);
       }
       const SERVICE_ENDPOINT: string = process.env.SERVICE_ENDPOINT || 'xxx';
-      const response = await axios.get(SERVICE_ENDPOINT, {});
+      const response = await axios.get(SERVICE_ENDPOINT, {
+        headers: {
+          Authorization: process.env.TEST_APIGATEWAY_ID_TOKEN
+        }
+      });
       expect(response.data).toBe('Hello World');
     } catch (e) {
       console.error('Integration Test Error: ' + e);
