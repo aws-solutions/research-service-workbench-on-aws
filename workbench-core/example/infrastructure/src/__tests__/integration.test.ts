024987f58bf6b05c08224f1f4a86dd7979ecb4ce
Samir Sharan <insignias@users.noreply.github.com>
2022-05-05 19:38:42 -0400

    fix: cognito token fix

--- workbench-core/example/infrastructure/src/__tests__/integration.test.ts
+++ workbench-core/example/infrastructure/src/__tests__/integration.test.ts
@@ -1,21 +1,39 @@
 import axios from 'axios';
+import _ from 'lodash';
+import { data } from './cognito-config';
 
 describe('IntegrationTest', () => {
   test("Service returns 'Hello World'", async () => {
     try {
-      if (process.env.SERVICE_ENDPOINT) {
-        console.log(process.env.SERVICE_ENDPOINT);
-      }
-      const SERVICE_ENDPOINT: string = process.env.SERVICE_ENDPOINT!;
-      const response = await axios.get(SERVICE_ENDPOINT, {
+      const body = JSON.stringify(data);
+      const region = process.env.AWS_DEV_REGION;
+      const url = `https://cognito-idp.${region}.amazonaws.com`;
+
+      /**
+       * Post request to initiateAuth
+       *
+       * @param url     - cognito idp url for your region
+       * @param body    - request body
+       * @param headers - request headers
+       *
+       */
+      const initiateAuthResponse = await axios.post(url, body, {
         headers: {
-          Authorization: process.env.TEST_APIGATEWAY_ID_TOKEN!
+          'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
+          'Content-Type': 'application/x-amz-json-1.1'
+        }
+      });
+
+      const idToken = _.get(initiateAuthResponse.data.AuthenticationResult, 'IdToken');
+
+      const response = await axios.get(process.env.SERVICE_ENDPOINT!, {
+        headers: {
+          Authorization: `${idToken}`
         }
       });
       expect(response.data).toBe('Hello World');
-    } catch (e) {
-      console.error('Integration Test Error: ' + e);
-      throw e;
+    } catch (error) {
+      console.log(error);
     }
   });
 });
