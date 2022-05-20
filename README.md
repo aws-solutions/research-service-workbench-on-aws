7debe54a78662327c24debd125a336317b20557d
Clara Mae Wells <wells.cl@husky.neu.edu>
2022-05-20 14:24:03 -0500

    feat: UI status-based button state, date-based filtering, split panel, 5/10 SA requests (#131)
    * feat(`solutions/swb-app`): create and update base ui files
    
    * fix: align create workspace button correctly, add dummy data to table
    
    * fix: fix nav text, remove unneeded consts
    
    * feat: add role-based navigation
    
    * feat: add table property filtering, add pagination
    
    * fix: correctly align create workspace button
    
    * feat(component): login page
    
    * fix: fix UI build issues
    
    * fix: correct filtering options, remove commented code
    
    * fix: correct status-based button states, fix rush errors
    
    * fix: fix dependency mis-matches
    
    * fix: separate swb-ui from swb-app
    
    * feat: add split panel, add date filter, implement SA meeting changes
    
    * fix: add files for date range filter
    
    * feat: control common level buttons based on workspace status
    
    * feat: add date filter functionality
    
    * docs: fix documentation, remove unused import
    
    * Add note regarding dummy data
    
    Co-authored-by: Clara <welclara@amazon.com>
    Co-authored-by: Kevin Park <kevpark@amazon.com>

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 | Statements                  | Branches                | Functions                 | Lines             |
 | --------------------------- | ----------------------- | ------------------------- | ----------------- |
 | ![Statements](https://img.shields.io/badge/statements-90.16%25-brightgreen.svg?style=flat) | ![Branches](https://img.shields.io/badge/branches-82.48%25-yellow.svg?style=flat) | ![Functions](https://img.shields.io/badge/functions-92.83%25-brightgreen.svg?style=flat) | ![Lines](https://img.shields.io/badge/lines-90.82%25-brightgreen.svg?style=flat) |
+
 # Contributing Guidelines
 
 Thank you for your interest in contributing to our project. Whether it's a bug report, new feature, correction, or additional documentation, we greatly value feedback and contributions from our community.
--- solutions/swb-ui/src/common/tableEmptyState.tsx
+++ solutions/swb-ui/src/common/tableEmptyState.tsx
@@ -2,24 +2,24 @@ import { Box, Button } from '@awsui/components-react';
 
 /**
  * Displays the empty state of any table
- * @param item - type of item that the table displays
+ * @param itemType - type of item that the table displays
  * @example
  * ```
  * <Table empty={ TableEmptyDisplay("workspace") } />
  * // Displays "No workspaces" title
  * // "No workspaces to display."
- * // <Button> with "Create workspace"
+ * // <Button> with "Create workspace" text
  * ```
  * @returns empty table information and call to action
  */
-export function TableEmptyDisplay(item: string) {
+export function TableEmptyDisplay(itemType: string): JSX.Element {
   return (
     <Box textAlign="center" color="inherit">
-      <b>No {item}s</b>
+      <b>No {itemType}s</b>
       <Box padding={{ bottom: 's' }} variant="p" color="inherit">
-        No {item}s to display.
+        No {itemType}s to display.
       </Box>
-      <Button>Create {item}</Button>
+      <Button>Create {itemType}</Button>
     </Box>
   );
 }
