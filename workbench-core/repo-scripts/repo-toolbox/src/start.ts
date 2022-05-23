2c8a04d5e6baf636e5a1e4ca9ba540f97b4206eb
Samir Sharan <insignias@users.noreply.github.com>
2022-05-23 18:44:15 -0400

    chore: comment fix

--- workbench-core/repo-scripts/repo-toolbox/src/start.ts
+++ workbench-core/repo-scripts/repo-toolbox/src/start.ts
@@ -1,8 +1,3 @@
-/**
- *
- * This code is a derivative of the code from https://github.com/microsoft/rushstack/blob/main/repo-scripts/repo-toolbox/src/ReadmeAction.ts
- *
- */
 import { ToolBoxCommandLine } from './toolboxCommandLine';
 
 const commandLine: ToolBoxCommandLine = new ToolBoxCommandLine();
