cfc0430d3f8b7cd61357803eb5d87397baf0b0e6
Tim Nguyen <thingut@amazon.com>
2022-05-26 16:52:00 -0400

    chore: update outputs

--- solutions/swb-reference/src/SWBStack.ts
+++ solutions/swb-reference/src/SWBStack.ts
@@ -358,7 +358,7 @@ export class SWBStack extends Stack {
       })
     );
 
-    new CfnOutput(this, 'accountHandlerLambdaRoleOutput', {
+    new CfnOutput(this, 'AccountHandlerLambdaRoleOutput', {
       value: lambda.role!.roleArn
     });
 
@@ -425,7 +425,7 @@ export class SWBStack extends Stack {
       })
     );
 
-    new CfnOutput(this, 'apiLambdaRoleOutput', {
+    new CfnOutput(this, 'ApiLambdaRoleOutput', {
       value: apiLambda.role!.roleArn
     });
 
