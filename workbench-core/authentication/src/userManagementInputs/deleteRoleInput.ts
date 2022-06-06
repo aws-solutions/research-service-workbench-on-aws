b5aeba11f6bfbb80bf5794c4f2467173e93f1848
HaiTao Zhang <haitaozhang246@gmail.com>
2022-06-06 15:59:56 -0700

    refactor(usermanagementinputs): extract inputs to allow for extensibility (#160)
    Co-authored-by: HaiTao Zhang <htzhang@amazon.com>

---
+++ workbench-core/authentication/src/userManagementInputs/deleteRoleInput.ts
@@ -0,0 +1,9 @@
+/**
+ * Input for deleteRole.
+ */
+export interface DeleteRoleInput {
+  /**
+   * The role to remove.
+   */
+  role: string;
+}
---
+++ workbench-core/authentication/src/userManagementInputs/deleteUserInput.ts
@@ -0,0 +1,9 @@
+/**
+ * Input for deleteUser.
+ */
+export interface DeleteUserInput {
+  /**
+   * The ID of the user to delete.
+   */
+  uid: string;
+}
