b44237799125e54995a9510cbe97462a5d9eba1f
David Madsen <dmmadse@amazon.com>
2022-06-07 15:34:19 -0700

    feat(s3datasetstroageplugin.ts): add manipulation of bucket policy

--- workbench-core/datasets/src/s3DataSetStoragePlugin.ts
+++ workbench-core/datasets/src/s3DataSetStoragePlugin.ts
@@ -1,13 +1,14 @@
 import { AwsService } from '@amzn/workbench-core-base';
-import { PutObjectCommandInput } from '@aws-sdk/client-s3';
+import { AnyPrincipal, Effect, PolicyDocument, PolicyStatement } from '@aws-cdk/aws-iam';
 import {
-  CreateAccessPointCommand,
-  CreateAccessPointCommandInput,
-  CreateAccessPointCommandOutput,
-  S3ControlClient,
-  S3ControlClientConfig
-} from '@aws-sdk/client-s3-control';
+  GetBucketPolicyCommandInput,
+  GetBucketPolicyCommandOutput,
+  PutBucketPolicyCommandInput,
+  PutObjectCommandInput
+} from '@aws-sdk/client-s3';
+import { CreateAccessPointCommandInput, CreateAccessPointCommandOutput } from '@aws-sdk/client-s3-control';
 import { Credentials } from '@aws-sdk/types';
+import _ from 'lodash';
 import { DataSetsStoragePlugin } from '.';
 
 /**
@@ -16,8 +17,7 @@ import { DataSetsStoragePlugin } from '.';
 export class S3DataSetStoragePlugin implements DataSetsStoragePlugin {
   private _aws: AwsService;
   private _kmsKeyArn: string;
-  private _s3control?: S3ControlClient;
-  private _credentials: Credentials;
+
   /**
    *
    * @param s3Options - options needed to create and maintain the S3 bucket associated with this provider.
@@ -25,7 +25,6 @@ export class S3DataSetStoragePlugin implements DataSetsStoragePlugin {
   public constructor(s3Options: { region: string; credentials: Credentials; kmsKeyArn: string }) {
     this._aws = new AwsService({ region: s3Options.region, credentials: s3Options.credentials });
     this._kmsKeyArn = s3Options.kmsKeyArn;
-    this._credentials = s3Options.credentials;
   }
 
   /**
@@ -54,26 +53,28 @@ export class S3DataSetStoragePlugin implements DataSetsStoragePlugin {
     externalEndpointName: string,
     externalRoleName: string
   ): Promise<string> {
-    if (!this._s3control) {
-      const config: S3ControlClientConfig = {
-        credentials: this._credentials
-      };
-      this._s3control = new S3ControlClient(config);
-    }
+    const accessPointUri: string = await this._createAccessPoint(name, externalEndpointName);
 
-    //const accessPointConfig: CreateAccessPointCommandInput = {
+    // apply a control delegation policy to the bucket.
 
-    //}
-    //const accessPointCommand: CreateAccessPointCommand = new CreateAccessPointCommand()
+    // apply bucket policy for the external URI.
 
-    return 'done!';
+    return accessPointUri;
   }
 
-  public async updateExternalEndpoint(
-    dataSetName: string,
+  public async addRoleToExternalEndpoint(
+    name: string,
     externalEndpointName: string,
     externalRoleName: string
   ): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+
+  public async removeRoleFromExternalEndpoint(
+    name: string,
+    externalEndpointName: string,
+    externalRoleArn: string
+  ): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
@@ -96,14 +97,101 @@ export class S3DataSetStoragePlugin implements DataSetsStoragePlugin {
     throw new Error('Method not implemented.');
   }
 
-  private async createAccessPoint(name: string, externalEndpointName: string): Promise<void> {}
+  private async _createAccessPoint(name: string, externalEndpointName: string): Promise<string> {
+    const accessPointConfig: CreateAccessPointCommandInput = {
+      Name: externalEndpointName,
+      Bucket: name
+    };
+    const response: CreateAccessPointCommandOutput = await this._aws.clients.s3Control.createAccessPoint(
+      accessPointConfig
+    );
+    return `s3://${response.AccessPointArn}`;
+  }
 
-  private async configureBucketPolicy(name: string, accessPointArn: string): Promise<void> {}
+  private async _configureBucketPolicy(name: string, accessPointArn: string): Promise<void> {
+    const getBucketPolicyConfig: GetBucketPolicyCommandInput = {
+      Bucket: name
+    };
+    const s3BucketArn: string = `arn:aws:s3:::${name}`;
+    const accountId: string = this._awsAccountIdFromArn(accessPointArn);
+    const delegationStatement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      principals: [new AnyPrincipal()],
+      actions: ['*'], // this will be limted in the access point policy.
+      resources: [s3BucketArn, `${s3BucketArn}/*`],
+      conditions: {
+        StringEquals: {
+          's3:DataAccessPointAccount': accountId
+        }
+      }
+    });
 
-  private async configureAccessPointPolicy(
+    const bucketPolicyResponse: GetBucketPolicyCommandOutput = await this._aws.clients.s3.getBucketPolicy(
+      getBucketPolicyConfig
+    );
+    let bucketPolicy: PolicyDocument;
+    if (bucketPolicyResponse.Policy) {
+      bucketPolicy = PolicyDocument.fromJson(bucketPolicyResponse.Policy);
+    } else {
+      bucketPolicy = new PolicyDocument();
+    }
+
+    if (this._bucketPolicyContainsDelegationStatement(bucketPolicy, accountId)) return;
+
+    bucketPolicy.addStatements(delegationStatement);
+
+    const putPolicyParams: PutBucketPolicyCommandInput = {
+      Bucket: name,
+      Policy: bucketPolicy.toString()
+    };
+
+    await this._aws.clients.s3.putBucketPolicy(putPolicyParams);
+  }
+
+  private async _configureAccessPointPolicy(
     name: string,
     dataSetPrefix: string,
     accessPointArn: string,
     externalRoleArn: string
   ): Promise<void> {}
+
+  private _awsAccountIdFromArn(arn: string): string {
+    const arnParts = arn.split(':');
+    if (arnParts.length < 6) {
+      throw new Error("Expected an arn with at least six ':' sepearted values.");
+    }
+
+    if (arnParts[4] === '') {
+      throw new Error('Expected an arn with an AWS AccountID however AWS AccountID field is empty.');
+    }
+
+    return arnParts[4];
+  }
+
+  private _bucketPolicyContainsDelegationStatement(policy: PolicyDocument, accountId: string): boolean {
+    const policyObj = policy.toJSON();
+    _.map(policyObj.Statements, (s) => {
+      if (this._isBucketDelegationStatement(PolicyStatement.fromJson(s), accountId)) return true;
+    });
+
+    return false;
+  }
+
+  private _isBucketDelegationStatement(statement: PolicyStatement, accountId: string): boolean {
+    // principal should be the "AWS: *"
+    if (!statement.hasPrincipal) return false;
+    if (!statement.principals.includes(new AnyPrincipal())) return false;
+    if (statement.effect !== Effect.ALLOW) return false;
+    if (statement.actions.length !== 1) return false;
+    if (!statement.actions.includes('*')) return false;
+
+    const conditions = JSON.parse(statement.conditions);
+    const stringEqualsCondition = _.get(conditions, 'StringEquals');
+    if (!stringEqualsCondition) return false;
+    const condReg = new RegExp('^\\s*s3:DataAccessPointAccountss*:\\s*\\d{12}\\s*$');
+    const matches = stringEqualsCondition.toString().match(condReg);
+    if (!matches) return false;
+    if (_.find(matches, (m) => m === accountId)) return true;
+    return false;
+  }
 }
