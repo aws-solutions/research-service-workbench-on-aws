2635de7b541482db1f82be00aa71e7eb2b3ffc07
Brendon Hudnell <bhudnell@amazon.com>
2022-06-08 17:31:33 -0600

    feat: added defaults for the UserPool and UserPoolClient constructs

--- workbench-core/infrastructure/src/cognitoInfrastructure.ts
+++ workbench-core/infrastructure/src/cognitoInfrastructure.ts
@@ -1,21 +1,40 @@
 import {
   AccountRecovery,
+  Mfa,
   OAuthScope,
   UserPool,
   UserPoolClient,
-  UserPoolClientIdentityProvider,
   UserPoolClientOptions,
   UserPoolClientProps,
   UserPoolProps
 } from 'aws-cdk-lib/aws-cognito';
-import { RemovalPolicy } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 
 import merge from 'lodash/merge';
 
-const userPoolDefaults: UserPoolProps = {};
+const userPoolDefaults: UserPoolProps = {
+  accountRecovery: AccountRecovery.EMAIL_ONLY,
+  enableSmsRole: false,
+  mfa: Mfa.OFF,
+  selfSignUpEnabled: false, // only admin can create users
+  signInAliases: {
+    // only sign in with email
+    username: false,
+    email: true
+  },
+  signInCaseSensitive: false
+};
 
+/**
+ * Defines a Cognito User Pool with Workbech defaults
+ */
 export class WorkbenchUserPool extends UserPool {
+  /**
+   *
+   * @param scope - the scope of the user pool
+   * @param id - the id of the user pool
+   * @param props - the {@link UserPoolProps} object
+   */
   public constructor(scope: Construct, id: string, props?: UserPoolProps) {
     const completeProps = merge(props, userPoolDefaults);
 
@@ -26,19 +45,30 @@ export class WorkbenchUserPool extends UserPool {
 const userPoolClientDefaults: UserPoolClientOptions = {
   generateSecret: true,
   authFlows: {
-    // TODO
+    adminUserPassword: true,
+    custom: true,
+    userSrp: true
   },
   oAuth: {
     flows: {
       authorizationCodeGrant: true
-    }, // TODO callback and logout URLSs?
+    },
     scopes: [OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PHONE]
   },
   preventUserExistenceErrors: true,
   enableTokenRevocation: true
 };
 
+/**
+ * Defines a Cognito User Pool Client with Workbech defaults
+ */
 export class WorkbenchUserPoolClient extends UserPoolClient {
+  /**
+   *
+   * @param scope - the scope of the user pool
+   * @param id - the id of the user pool
+   * @param props - the {@link UserPoolClientProps} object
+   */
   public constructor(scope: Construct, id: string, props: UserPoolClientProps) {
     const completeProps = merge(props, userPoolClientDefaults);
 
