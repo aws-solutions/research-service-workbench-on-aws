44774b34d5c626c6a85077f4696e989ed99989e8
HaiTao Zhang <haitaozhang246@gmail.com>
2022-06-23 01:58:57 -0700

    feat(audit): auditMiddlewware implementation added with masking added to AuditService

--- workbench-core/audit/src/index.ts
+++ workbench-core/audit/src/index.ts
@@ -3,3 +3,6 @@ export { default as BaseAuditPlugin } from './plugins/baseAuditPlugin';
 export { default as AuditEntry } from './auditEntry';
 export { default as AuditPlugin } from './auditPlugin';
 export { default as Writer } from './plugins/writer';
+export { Extractor } from './extractor';
+export { BaseExtractor } from './baseExtractor';
+export { WithAudit } from './auditMiddleware';
