994cb7ac17d0c77e3b3e2ff8f9b5b8892c7aa9be
Karina Cadette <swe.cadette@gmail.com>
2022-06-24 08:36:53 -0500

    feat(managed-blockchain-for-patient-consent): renmove express app from client app
    there is no logic in the express project at this stage.

--- solutions/managed-blockchain-for-patient-consent/patient-consent-infrastructure/src/init.ts
+++ solutions/managed-blockchain-for-patient-consent/patient-consent-infrastructure/src/init.ts
@@ -1,8 +1,6 @@
-import { mainRouter } from '@amzn/patient-consent-express';
 import serverlessExpress from '@vendia/serverless-express';
 import express, { Express } from 'express';
 
 const app: Express = express();
-app.use('/', mainRouter);
 
 exports.handler = serverlessExpress({ app });
