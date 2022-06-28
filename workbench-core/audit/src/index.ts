646d834f48db7217dcafbc6c85a8dea002930854
HaiTao Zhang <haitaozhang246@gmail.com>
2022-06-27 17:16:51 -0700

    refactor(audit): export metadata

--- workbench-core/audit/src/index.ts
+++ workbench-core/audit/src/index.ts
@@ -3,6 +3,7 @@ export { default as BaseAuditPlugin } from './plugins/baseAuditPlugin';
 export { default as AuditEntry } from './auditEntry';
 export { default as AuditPlugin } from './auditPlugin';
 export { default as Writer } from './plugins/writer';
+export { default as Metadata } from './metadata';
 export { Extractor } from './extractor';
 export { BaseExtractor } from './baseExtractor';
 export { WithAudit } from './auditMiddleware';
