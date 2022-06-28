14c4d937b2537dc79228e508bb2d94224cd7471f
Brendon Hudnell <bhudnell@amazon.com>
2022-06-28 11:20:36 -0600

    feat: added 3rd party idp integration, removed unnecessary permissions, updated aws-cdk-lib version

--- workbench-core/infrastructure/src/cognitoInfrastructure.ts
+++ workbench-core/infrastructure/src/cognitoInfrastructure.ts
@@ -1,16 +1,19 @@
 import {
   AccountRecovery,
-  IUserPoolIdentityProvider,
   Mfa,
   OAuthScope,
   UserPool,
   UserPoolClient,
   UserPoolClientOptions,
   UserPoolDomain,
+  UserPoolIdentityProviderOidc,
+  UserPoolIdentityProviderOidcProps,
   UserPoolProps
 } from 'aws-cdk-lib/aws-cognito';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { CfnOutput, SecretValue, Stack } from 'aws-cdk-lib/core';
+import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
 import { Construct } from 'constructs';
-
 import merge from 'lodash/merge';
 
 const userPoolDefaults: UserPoolProps = {
@@ -39,16 +42,11 @@ const userPoolDefaults: UserPoolProps = {
 
 const userPoolClientDefaults: UserPoolClientOptions = {
   generateSecret: true,
-  authFlows: {
-    adminUserPassword: true,
-    custom: true,
-    userSrp: true
-  },
   oAuth: {
     flows: {
       authorizationCodeGrant: true
     },
-    scopes: [OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PHONE]
+    scopes: [OAuthScope.OPENID]
   },
   preventUserExistenceErrors: true,
   enableTokenRevocation: true
@@ -58,8 +56,11 @@ export interface WorkbenchCognitoProps {
   domainPrefix: string;
   websiteUrl: string;
   userPoolName?: string;
-  identityProviders?: IUserPoolIdentityProvider[];
+  oidcIdentityProviders?: WorkbenchUserPoolOidcIdentityProvider[];
 }
+
+export interface WorkbenchUserPoolOidcIdentityProvider
+  extends Omit<UserPoolIdentityProviderOidcProps, 'userPool'> {}
 
 export class WorkbenchCognito extends Construct {
   public readonly userPool: UserPool;
@@ -67,14 +68,30 @@ export class WorkbenchCognito extends Construct {
   public readonly userPoolDomain: UserPoolDomain;
 
   public constructor(scope: Construct, id: string, props: WorkbenchCognitoProps) {
-    const { domainPrefix, websiteUrl, userPoolName, identityProviders } = props;
+    const {
+      domainPrefix,
+      websiteUrl,
+      userPoolName,
+      oidcIdentityProviders: oidcIdentityProviderProps
+    } = props;
     super(scope, id);
+
+    const region = Stack.of(this).region;
 
     this.userPool = new UserPool(this, 'WorkbenchUserPool', { ...userPoolDefaults, userPoolName });
 
     this.userPoolDomain = new UserPoolDomain(this, 'WorkbenchUserPoolDomain', {
       userPool: this.userPool,
       cognitoDomain: { domainPrefix: domainPrefix }
+    });
+
+    oidcIdentityProviderProps?.forEach((props) => {
+      const provider = new UserPoolIdentityProviderOidc(this, 'WorkbenchUserPoolIdentityProviderOidc', {
+        ...props,
+        userPool: this.userPool,
+        scopes: ['openid', 'profile', 'email']
+      });
+      this.userPool.registerIdentityProvider(provider);
     });
 
     const tempProps: UserPoolClientOptions = {
@@ -89,6 +106,35 @@ export class WorkbenchCognito extends Construct {
       userPool: this.userPool
     });
 
-    identityProviders?.forEach((provider) => this.userPool.registerIdentityProvider(provider));
+    this.userPool.identityProviders.forEach((provider) => this.userPoolClient.node.addDependency(provider));
+
+    const describeCognitoUserPoolClient = new AwsCustomResource(this, 'DescribeCognitoUserPoolClient', {
+      resourceType: 'Custom::DescribeCognitoUserPoolClient',
+      onCreate: {
+        region,
+        service: 'CognitoIdentityServiceProvider',
+        action: 'describeUserPoolClient',
+        parameters: {
+          UserPoolId: this.userPool.userPoolId,
+          ClientId: this.userPoolClient.userPoolClientId
+        },
+        physicalResourceId: PhysicalResourceId.of(this.userPoolClient.userPoolClientId)
+      },
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [this.userPool.userPoolArn]
+      })
+    });
+
+    const userPoolClientSecret = describeCognitoUserPoolClient.getResponseField(
+      'UserPoolClient.ClientSecret'
+    );
+
+    new CfnOutput(this, 'WorkbenchUserPoolDomainName', { value: this.userPoolDomain.baseUrl() });
+    new CfnOutput(this, 'WorkbenchUserPoolId', { value: this.userPool.userPoolId });
+    new CfnOutput(this, 'WorkbenchUserPoolClientId', { value: this.userPoolClient.userPoolClientId });
+    new Secret(this, 'WorkbenchUserPoolClientSecret', {
+      secretName: 'WorkbenchUserPoolClientSecret',
+      secretStringValue: SecretValue.unsafePlainText(userPoolClientSecret)
+    });
   }
 }
