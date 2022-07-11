f7f61cfeda82b087a83102248182bb3128f628ec
Tim Nguyen <thingut@amazon.com>
2022-07-10 23:55:45 -0400

    chore: lowercase id

--- workbench-core/datasets/src/ddbDataSetMetadataPlugin.ts
+++ workbench-core/datasets/src/ddbDataSetMetadataPlugin.ts
@@ -134,7 +134,7 @@ export class DdbDataSetMetadataPlugin implements DataSetMetadataPlugin {
   private async _storeEndPointToDdb(endPoint: ExternalEndpoint): Promise<string> {
     const endPointKey = {
       pk: `${this._dataSetKeyType}#${endPoint.dataSetId}`,
-      sk: `${this._endPointKeyType}#${endPoint.Id}`
+      sk: `${this._endPointKeyType}#${endPoint.id}`
     };
     const endPointParams: { item: { [key: string]: string | string[] } } = {
       item: {
