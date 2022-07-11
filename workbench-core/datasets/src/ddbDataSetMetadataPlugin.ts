053be563ebf0ba0fa550acaf585c18b51c552dba
Tim Nguyen <thingut@amazon.com>
2022-07-10 23:30:20 -0400

    fix: lowercase id

--- workbench-core/datasets/src/ddbDataSetMetadataPlugin.ts
+++ workbench-core/datasets/src/ddbDataSetMetadataPlugin.ts
@@ -81,7 +81,7 @@ export class DdbDataSetMetadataPlugin implements DataSetMetadataPlugin {
   public async addExternalEndpoint(endPoint: ExternalEndpoint): Promise<ExternalEndpoint> {
     const endPointParam: ExternalEndpoint = endPoint;
     await this._validateCreateExternalEndpoint(endPoint);
-    endPointParam.Id = uuidv4();
+    endPointParam.id = uuidv4();
     if (_.isUndefined(endPointParam.createdAt)) endPointParam.createdAt = new Date().toISOString();
     await this._storeEndPointToDdb(endPointParam);
     return endPointParam;
