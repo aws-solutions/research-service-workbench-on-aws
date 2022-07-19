1b4790555d24f732ca33900a8025b20ffc053ada
David Madsen <dmmadse@amazon.com>
2022-07-19 15:04:35 -0700

    fix(createaccountschema.ts): remove a few fields that don't make sense on create

--- workbench-core/environments/src/schemas/createAccount.ts
+++ workbench-core/environments/src/schemas/createAccount.ts
@@ -5,25 +5,15 @@ const CreateAccountSchema: Schema = {
   id: '/createAccount',
   type: 'object',
   properties: {
-    id: { type: 'string' },
     awsAccountId: { type: 'string' },
     envMgmtRoleArn: { type: 'string' },
-    error: {
-      type: 'object',
-      properties: {
-        type: { type: 'string' },
-        value: { type: 'string' }
-      }
-    },
     hostingAccountHandlerRoleArn: { type: 'string' },
     vpcId: { type: 'string' },
     subnetId: { type: 'string' },
     cidr: { type: 'string' },
     environmentInstanceFiles: { type: 'string' },
     encryptionKeyArn: { type: 'string' },
-    externalId: { type: 'string' },
-    stackName: { type: 'string' },
-    status: { type: 'string' }
+    externalId: { type: 'string' }
   },
   required: [
     'awsAccountId',
