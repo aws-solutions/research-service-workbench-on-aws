9d9ab96ebdd74c5b4b3ec72586f4aa72730f209f
Felipe Rangel <rangfeli@amazon.com>
2022-07-25 11:33:29 -0400

    chore(file name): added comments to updatePlugins.sh

--- solutions/swb-ui/scripts/updatePlugins.sh
+++ solutions/swb-ui/scripts/updatePlugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Starting Update of Plugins";
 cd ./solutions/swb-plugins;
-for plugin in *; do
-    if [[ "$(ls -l .)" == "total 0" ]]; then
+for plugin in *; do #Iterate through swb-plugins
+    if [[ "$(ls -l .)" == "total 0" ]]; then #Check to see if swb-plugins directory is empty
         echo "No Plugins Detected";
         break;
     fi
-    if [ -d "$plugin" ]; then
+    if [ -d "$plugin" ]; then #Check to see if item is a directory
         pluginPackageName=$(jq -r '.name' ./$plugin/package.json);
         pluginUpper=$(echo "${plugin}" | tr “[a-z]” “[A-Z]”);
         if ! $(jq --arg pluginName $pluginPackageName 'any(.projects[].packageName == $pluginName; .)' ../../rush.json); then
@@ -29,28 +29,28 @@ for plugin in *; do
         fi
     fi
 done
-for page in ../swb-ui/src/pages/apps/*; do
-    if [[ $page == *.tsx ]]; then
+for page in ../swb-ui/src/pages/apps/*; do #Iterate through all items in pages/apps
+    if [[ $page == *.tsx ]]; then #Check to see if item is a .tsx file
         pluginExists="false";        
-        for plugin in *; do
-            if [[ "$(ls -l .)" == "total 0" ]]; then
+        for plugin in *; do #Iterate through swb-plugins
+            if [[ "$(ls -l .)" == "total 0" ]]; then #Check to see if swb-plugins directory is empty
                 pluginExists="false";
                 break;
             fi
-            if [ -d "$plugin" ]; then
-                if [[ $plugin == $(basename $page .tsx) ]]; then
+            if [ -d "$plugin" ]; then #Check to see if item is a directory
+                if [[ $plugin == $(basename $page .tsx) ]]; then #Check to see if plugin has same name as .tsx file
                     pluginExists="true";
                 fi
             fi
         done
         echo "Plugin for $(basename $page) Exists: $pluginExists";
-        if [[ $pluginExists == "false" ]]; then
+        if [[ $pluginExists == "false" ]]; then #Proceed to clean up files associated with plugin
             pluginPackageName=$(head -n 1 $page | grep -o "'.*'" | tr -d "'");
             jq --arg pluginName $pluginPackageName 'del(.projects[] | select(.packageName == $pluginName))' ../../rush.json > ../../output_rush.json;
             mv ../../output_rush.json  ../../rush.json;
             jq 'del(.dependencies."'"$pluginPackageName"'")' ../swb-ui/package.json > ../swb-ui/output_package.json;
             mv ../swb-ui/output_package.json ../swb-ui/package.json;
-            rm $page;
+            rm $page; #Delete page associated with plugin
         fi
     fi
 done 
