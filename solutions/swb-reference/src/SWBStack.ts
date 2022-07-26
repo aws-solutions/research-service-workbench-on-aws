e4049858719e4fdc9a317c61b1f4321cd7e22340
Kevin Park <kevpark@amazon.com>
2022-07-26 14:22:13 -0700

    feat(swb-reference): add API Gateway access logging

--- solutions/swb-reference/src/SWBStack.ts
+++ solutions/swb-reference/src/SWBStack.ts
@@ -9,11 +9,15 @@ import { join } from 'path';
 import { WorkbenchCognito, WorkbenchCognitoProps } from '@amzn/workbench-core-infrastructure';
 
 import { App, CfnOutput, Duration, Stack } from 'aws-cdk-lib';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import {
+  AccessLogFormat,
+  LambdaIntegration,
+  LogGroupLogDestination,
+  RestApi
+} from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
-
 import {
   AccountPrincipal,
   AnyPrincipal,
@@ -26,6 +30,7 @@ import {
 } from 'aws-cdk-lib/aws-iam';
 import { Key } from 'aws-cdk-lib/aws-kms';
 import { Alias, Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import _ from 'lodash';
 import { getConstants } from './constants';
@@ -719,11 +724,28 @@ export class SWBStack extends Stack {
 
   // API Gateway
   private _createRestApi(apiLambda: Function): void {
+    const logGroup = new LogGroup(this, 'APIGatewayAccessLogs');
     const API: RestApi = new RestApi(this, `API-Gateway API`, {
       restApiName: 'Backend API Name',
       description: 'Backend API',
       deployOptions: {
-        stageName: 'dev'
+        stageName: 'dev',
+        accessLogDestination: new LogGroupLogDestination(logGroup),
+        accessLogFormat: AccessLogFormat.custom(
+          JSON.stringify({
+            stage: '$context.stage',
+            requestId: '$context.requestId',
+            integrationRequestId: '$context.integration.requestId',
+            status: '$context.status',
+            apiId: '$context.apiId',
+            resourcePath: '$context.resourcePath',
+            path: '$context.path',
+            resourceId: '$context.resourceId',
+            httpMethod: '$context.httpMethod',
+            sourceIp: '$context.identity.sourceIp',
+            userAgent: '$context.identity.userAgent'
+          })
+        )
       },
       defaultCorsPreflightOptions: {
         allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key'],
