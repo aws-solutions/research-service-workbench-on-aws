9369ed1a268cc7d47259d38cf01deb762a3e5018
Tim Nguyen <thingut@amazon.com>
2022-07-27 14:27:09 -0400

    chore: add async

--- solutions/swb-reference/scripts/generateCognitoTokens.js
+++ solutions/swb-reference/scripts/generateCognitoTokens.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 // To use the script, follow the example below
 // STAGE=<STAGE> generateSecretHash.js <userName> '<password>'
 
@@ -8,40 +14,47 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const { AwsService } = require('@amzn/workbench-core-base');
 
-const config = yaml.load(
-  // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
-  // correct file
-  // eslint-disable-next-line security/detect-non-literal-fs-filename
-  fs.readFileSync(join(__dirname, `../integration-tests/config/${process.env.STAGE}.yaml`), 'utf8') // nosemgrep
-);
+async function run() {
+  const config = yaml.load(
+    // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
+    // correct file
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    fs.readFileSync(join(__dirname, `../integration-tests/config/${process.env.STAGE}.yaml`), 'utf8') // nosemgrep
+  );
 
-const clientSecret = config.clientSecret;
-const clientId = config.clientId;
-const userPoolId = config.userPoolId;
-const region = config.awsRegion;
-const username = process.argv[2];
-const password = process.argv[3];
+  const clientSecret = config.clientSecret;
+  const clientId = config.clientId;
+  const userPoolId = config.userPoolId;
+  const region = config.awsRegion;
+  const username = process.argv[2];
+  const password = process.argv[3];
 
-const secretHash = crypto
-  .createHmac('SHA256', clientSecret)
-  .update(username + clientId)
-  .digest('base64');
+  const secretHash = crypto
+    .createHmac('SHA256', clientSecret)
+    .update(username + clientId)
+    .digest('base64');
 
-const aws = new AwsService({
-  region
-});
-
-aws.clients.cognito
-  .adminInitiateAuth({
-    UserPoolId: userPoolId,
-    ClientId: clientId,
-    AuthFlow: 'ADMIN_NO_SRP_AUTH',
-    AuthParameters: {
-      USERNAME: username,
-      PASSWORD: password,
-      SECRET_HASH: secretHash
-    }
-  })
-  .then((response) => {
-    console.log(response.AuthenticationResult);
+  const aws = new AwsService({
+    region
   });
+
+  try {
+    const response = await aws.clients.cognito.adminInitiateAuth({
+      UserPoolId: userPoolId,
+      ClientId: clientId,
+      AuthFlow: 'ADMIN_NO_SRP_AUTH',
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+        SECRET_HASH: secretHash
+      }
+    });
+    console.log(response.AuthenticationResult);
+  } catch (e) {
+    console.error('Unable to get cognito tokens', e);
+  }
+}
+
+(async () => {
+  await run();
+})();
