40d6b85f9a73de624bbeaf8ffa2a2571d6c028fb
Felipe Rangel <rangfeli@amazon.com>
2022-07-28 11:47:09 -0400

    fix(file name): apps_menu.js only considers now .tsx files

--- solutions/swb-ui/scripts/apps_menu.js
+++ solutions/swb-ui/scripts/apps_menu.js
@@ -2,5 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  get: (pagePath) => fs.readdirSync(pagePath).map((file) => path.parse(file).name)
+  get: (pagePath) =>
+    fs.readdirSync(pagePath).map((file) => {
+      if (path.extname(file).toLowerCase() === '.tsx') {
+        return path.parse(file).name;
+      }
+    })
 };
