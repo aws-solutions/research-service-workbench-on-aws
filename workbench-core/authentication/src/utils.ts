6fbe53399e60f070472527f50f2dc79970ba9b2c
Brendon Hudnell <bhudnell@amazon.com>
2022-08-09 13:21:46 -0600

    refactor: replaced utils dependency on aws sdk with own enum

--- workbench-core/authentication/src/utils.ts
+++ workbench-core/authentication/src/utils.ts
@@ -3,16 +3,21 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-import { TimeUnitsType } from '@aws-sdk/client-cognito-identity-provider';
+export enum TimeUnits {
+  DAYS = 'days',
+  HOURS = 'hours',
+  MINUTES = 'minutes',
+  SECONDS = 'seconds'
+}
 
-export function getTimeInSeconds(length: number, units: TimeUnitsType): number {
-  if (units === TimeUnitsType.DAYS) {
+export function getTimeInSeconds(length: number, units: TimeUnits): number {
+  if (units === TimeUnits.DAYS) {
     return length * 86400;
   }
-  if (units === TimeUnitsType.HOURS) {
+  if (units === TimeUnits.HOURS) {
     return length * 3600;
   }
-  if (units === TimeUnitsType.MINUTES) {
+  if (units === TimeUnits.MINUTES) {
     return length * 60;
   }
   return length;
