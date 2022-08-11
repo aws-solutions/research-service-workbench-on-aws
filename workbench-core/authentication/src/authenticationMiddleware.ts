acfaf45ae47c66d90a19479cce378ff8a9c7a753
Brendon Hudnell <bhudnell@amazon.com>
2022-08-11 15:01:28 -0600

    Update workbench-core/authentication/src/authenticationMiddleware.ts
    Co-authored-by: Robert Smayda <smayda@amazon.com>

--- workbench-core/authentication/src/authenticationMiddleware.ts
+++ workbench-core/authentication/src/authenticationMiddleware.ts
@@ -279,7 +279,7 @@ export function refreshAccessToken(
  *  - if there is a refresh token, it is stored in a cookie named `refresh_token`
  *
  * @param authenticationService - a configured {@link AuthenticationService} instance
- * @param options - an options object containing optional sameSite cookie and logging service parameters
+ * @param options - object containing optional sameSite cookie and logging service parameters
  * @returns the route handler function
  *
  * @example
