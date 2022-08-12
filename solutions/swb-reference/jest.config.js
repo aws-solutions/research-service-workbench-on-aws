f614bf2b9c48630e80378fd1d4e78f997d6cc85e
Sanket Dharwadkar <sdharwad@amazon.com>
2022-08-12 07:22:47 -0700

    test: integration test SWB env with DS (#453)
    * test: add complex test for DS SWB integration

--- solutions/swb-reference/jest.config.js
+++ solutions/swb-reference/jest.config.js
@@ -24,8 +24,25 @@ async function init() {
     fs.readFileSync(join(__dirname, `integration-tests/config/${stage}.yaml`), 'utf8')
   );
 
+  let outputs;
+  try {
+    const apiStackOutputs = JSON.parse(
+      fs.readFileSync(join(__dirname, `../src/config/${process.env.STAGE}.json`), 'utf8') // nosemgrep
+    );
+    const apiStackName = Object.entries(apiStackOutputs).map(([key, value]) => key)[0]; //output has a format { stackname: {...props} }
+    outputs = apiStackOutputs[apiStackName];
+  } catch (e) {
+    throw new Error(
+      'There was a problem reading the main stage file. Please run cdk-deploy prior to running the integration test suite'
+    );
+  }
+
+  const mainAccountId = outputs.dynamoDBTableOutput.split(':')[4];
+
   settings = {
     ...config,
+    ...outputs,
+    mainAccountId,
     runId: `${Date.now()}`
   };
 }
