999d3a4eba9c321c1cda3f270e40a3945c0a1f3b
Brendon Hudnell <bhudnell@amazon.com>
2022-08-15 11:33:15 -0600

    docs: added rush change files, added missing tsdoc

--- workbench-core/authentication/src/utils.ts
+++ workbench-core/authentication/src/utils.ts
@@ -10,6 +10,13 @@ export enum TimeUnits {
   SECONDS = 'seconds'
 }
 
+/**
+ * Converts a amount of time into milliseconds
+ *
+ * @param length - the length of time
+ * @param units - the unit of time
+ * @returns the time in milliseconds
+ */
 export function getTimeInMS(length: number, units: TimeUnits): number {
   if (units === TimeUnits.DAYS) {
     return length * 24 * 60 * 60 * 1000;
