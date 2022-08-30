110c9cd066eba2d19f184214254ca8a826c33bd3
FernandoAranda <fernandoarandacarrillo@gmail.com>
2022-08-30 09:43:55 -0700

    feat: cypress integration (#481)
    * feat(e2e test): add Cypress to swb-reference
    
    * chore(e2e tests): add rush changes
    
    * feat(e2e test): update documentation and fix cypress known issue
    
    * chore(e2e test): fix pipeline issue
    
    * chore(e2e tests): fix typos and remove wait methods in cypress
    
    * docs(e2e tests): fix typo
    
    * chore(e2e tests): add attribute search for profile dropdown menu
    
    * chore(e2e tests): remove unecessary validations
    
    Co-authored-by: Fernando Aranda <farandac@amazon.com>

---
+++ solutions/swb-reference/end-to-end-tests/cypress/support/constants.ts
@@ -0,0 +1,7 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+export const ADMIN_PASSWORD_PROPERTY: string = 'ADMIN_PASSWORD';
+export const ADMIN_USER_PROPERTY: string = 'ADMIN_USER';
+export const COGNITO_DOMAIN_NAME_PROPERTY: string = 'COGNITO_DOMAIN_NAME';
