edab679766ef2ffd03764395e02f0e9602d5245d
Sanket Dharwadkar <sdharwad@amazon.com>
2022-09-01 02:36:40 -0400

    fix: move datasets UI component to DS UI package

--- workbench-core/datasets-ui/src/index.tsx
+++ workbench-core/datasets-ui/src/index.tsx
@@ -8,6 +8,7 @@ import { columnDefinitions, searchableColumns } from './datasets-table-config/da
 import { filteringOptions } from './datasets-table-config/datasetsFilteringOptions';
 import { filteringProperties } from './datasets-table-config/datasetsFilteringProperties';
 import { type DatasetItem } from './models/Dataset';
+import { DatasetsPage } from './pages/index';
 
 
 export {
@@ -19,4 +20,7 @@ export {
 
   // From '/models' folder
   DatasetItem,
+
+  // From 'pages' folder
+  DatasetsPage
 };
