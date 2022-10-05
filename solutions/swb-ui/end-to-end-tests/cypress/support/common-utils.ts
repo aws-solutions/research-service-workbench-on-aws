e3a7b09d28d0817b33637c4d000eb16f7562e0eb
FernandoAranda <fernandoarandacarrillo@gmail.com>
2022-10-05 13:19:49 -0700

    chore: Move e2e to swb-ui (#506)
    * chore(e2e test): move e2e tests to swb-ui
    
    * chore(swb-ui deployment): merge export and build and fix issues with develop merge
    
    Co-authored-by: Fernando Aranda <farandac@amazon.com>

--- solutions/swb-ui/end-to-end-tests/cypress/support/common-utils.ts
+++ solutions/swb-ui/end-to-end-tests/cypress/support/common-utils.ts
@@ -2,17 +2,19 @@
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  */
-
-export const getFakeText = (text: string): string => {
-  return `${text}-${new Date().getTime()}-${getRandomNumberAsString()}`;
-};
-
 const getRandomNumberAsString = (): string => {
   const num = Math.round(Math.random() * 1000) + 1;
   return num.toString();
 };
 
-export function selectItemCard(componentTestId: string, itemToSelect: string) {
+export const escapeRegex = (expression: string): string => {
+  return expression.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+export const getFakeText = (text: string): string => {
+  return `${text}-${new Date().getTime()}-${getRandomNumberAsString()}`;
+};
+
+export function selectItemCard(componentTestId: string, itemToSelect: string): void {
   cy.get(`[data-testid="${componentTestId}"]`).should('be.visible');
   cy.get(`[data-testid="${itemToSelect}"]`).should('be.visible');
   cy.get(
@@ -20,16 +22,18 @@ export function selectItemCard(componentTestId: string, itemToSelect: string) {
   ).click();
 }
 
-export function selectItemDropDown(componentTestId: string, itemsToSelect: string[]) {
+export function selectItemDropDown(componentTestId: string, itemsToSelect: string[]): void {
   cy.get(`[data-testid="${componentTestId}"] button`).click();
-  itemsToSelect.forEach((item) => {
+  itemsToSelect.forEach((item): void => {
+    const itemRegex = `^${escapeRegex(item)}$`;
     cy.get(`[data-testid="${componentTestId}"]`)
-      .contains(new RegExp(`^${item}$`))
+      // eslint-disable-next-line security/detect-unsafe-regex, security/detect-non-literal-regexp, @rushstack/security/no-unsafe-regexp
+      .contains(new RegExp(itemRegex))
       .click();
   });
 }
 
-export function selectItemGrid(componentTestId: string, itemToSelect: string) {
+export function selectItemGrid(componentTestId: string, itemToSelect: string): void {
   cy.get(
     `[data-testid="${componentTestId}"] tr:has(div[data-testid="${itemToSelect}"]) td[class^="awsui_selection-contro"]:first`
   ).click();
@@ -39,11 +43,11 @@ export function selectItemGrid(componentTestId: string, itemToSelect: string) {
  * This function validates if a polaris table contains a column with specified header name, a row with specified data-testid and column value.
  * If column with value is not found within timeout it will throw a cypress validation error
  *
- * @param {string} componentTestId Data-testId attribute from Table
- * @param {string} rowTestId Data-testId attribute from row
- * @param {string} columnToVerify Name of column to validate
- * @param {string} valueToVerify Value of columnt to validate
- * @param {number} timeoutInMiliseconds Time limit in miliseconds to find value
+ * @param componentTestId -  Data-testId attribute from Table
+ * @param rowTestId -  Data-testId attribute from row
+ * @param columnToVerify - Name of column to validate
+ * @param valueToVerify -  Value of columnt to validate
+ * @param timeoutInMiliseconds -  Time limit in miliseconds to find value
  *******************************************************************************************************************************************************************/
 export function validateTableData(
   componentTestId: string,
@@ -51,18 +55,22 @@ export function validateTableData(
   columnToVerify: string,
   valueToVerify: string,
   timeoutInMiliseconds?: number
-) {
-  cy.contains(`[data-testId="${componentTestId}"] th`, new RegExp(`^${columnToVerify}$`))
+): void {
+  const columnNameRegex = `^${escapeRegex(columnToVerify)}$`;
+  const columnValueRegex = `^${escapeRegex(valueToVerify)}$`;
+  // eslint-disable-next-line security/detect-non-literal-regexp, @rushstack/security/no-unsafe-regexp
+  cy.contains(`[data-testId="${componentTestId}"] th`, new RegExp(columnNameRegex))
     .invoke('index')
     .then((i) => {
       cy.contains(
         `[data-testid="${componentTestId}"] tr:has(div[data-testid="${rowTestId}"]) td:nth-child(${i + 1})`,
-        new RegExp(`^${valueToVerify}$`),
+        // eslint-disable-next-line security/detect-unsafe-regex, security/detect-non-literal-regexp, @rushstack/security/no-unsafe-regexp
+        new RegExp(columnValueRegex),
         { timeout: timeoutInMiliseconds }
       );
     });
 }
 
-export function clickSelectAllGrid(componentTestId: string) {
+export function clickSelectAllGrid(componentTestId: string): void {
   cy.get(`[data-testid="${componentTestId}"] thead th:first`).click();
 }
