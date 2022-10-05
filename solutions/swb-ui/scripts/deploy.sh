17f12015697cc277194b6689e4a6888cd16b11d1
Fernando Aranda <farandac@amazon.com>
2022-10-04 17:51:36 -0700

    chore(swb-ui deployment): merge export and build and fix issues with develop merge

--- solutions/swb-ui/scripts/deploy.sh
+++ solutions/swb-ui/scripts/deploy.sh
@@ -5,7 +5,7 @@ then
     echo Configuration with STAGE='"'${STAGE}'"' not found, make sure to deploy API with STAGE=${STAGE} and try again.
     exit 1
 else
-    (NEXT_PUBLIC_API_BASE_URL=$apiURL rush build) && 
+    (rush build && NEXT_PUBLIC_API_BASE_URL=$apiURL rushx build) && ##rushx build is necessary as rush build does not take in count NEXT_PUBLIC_API_BASE_URL env variable for swb-ui build
     (
         cd infrastructure 
         rushx cdk bootstrap && rushx cdk-deploy
