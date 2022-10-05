ef339159d0f5e35f2ba6653c6fe7b89f1d44b8d9
HaiTao Zhang <haitaozhang246@gmail.com>
2022-10-04 17:58:52 -0700

    feat(authorization): additional changes to dynamicPermissionsPluginInputs

--- workbench-core/authorization/src/dynamicAuthorization/dynamicPermissionsPluginInputs.ts
+++ workbench-core/authorization/src/dynamicAuthorization/dynamicPermissionsPluginInputs.ts
@@ -67,6 +67,9 @@ export interface GetUserGroupsResponse {
   groupIds: string[];
 }
 
+/**
+ * Represents an Identity Permission
+ */
 export interface IdentityPermission {
   /**
    * {@link Identity}
@@ -89,6 +92,9 @@ export interface IdentityPermission {
  * Request object for DynamicPermissionsPlugin's createIdentityPermissions
  */
 export interface CreateIdentityPermissionsRequest {
+  /**
+   * An array of {@link IdentityPermission} to be created
+   */
   identityPermissions: IdentityPermission[];
 }
 
@@ -96,12 +102,18 @@ export interface CreateIdentityPermissionsRequest {
  * Response object for DynamicPermissionsPlugin's createIdentityPermissions
  */
 export interface CreateIdentityPermissionsResponse {
+  /**
+   * States whether the {@link IdentityPermission}s were successfully created
+   */
   created: boolean;
 }
 /**
  * Request object for DynamicPermissionsPlugin's deleteIdentityPermissions
  */
 export interface DeleteIdentityPermissionsRequest {
+  /**
+   * An array of {@link IdentityPermission} to be deleted
+   */
   identityPermissions: IdentityPermission[];
 }
 
@@ -109,28 +121,106 @@ export interface DeleteIdentityPermissionsRequest {
  * Response object for DynamicPermissionsPlugin's deleteIdentityPermissions
  */
 export interface DeleteIdentityPermissionsResponse {
-  created: boolean;
+  /**
+   * States whether the {@link IdentityPermission}s were successfully deleted
+   */
+  deleted: boolean;
 }
 
-export interface DeleteSubjectPermissionsRequest {}
+/**
+ * Request object for DynamicPermissionsPlugin's deleteSubjectPermissions
+ */
+export interface DeleteSubjectPermissionsRequest {
+  /**
+   * The subject to be deleted
+   */
+  subject: string;
 
-export interface DeleteSubjectPermissionsResponse {}
+  /**
+   * The subject id associated to the subject to be deleted
+   */
+  subjectId: string;
+}
+/**
+ * Response object for DynamicPermissionsPlugin's deleteSubjectPermissions
+ */
+export interface DeleteSubjectPermissionsResponse {
+  /**
+   * States whether all the subject's permissions were successfully created
+   */
+  deleted: boolean;
+}
+/**
+ * Request object for DynamicPermissionsPlugin's assignUserToGroup
+ */
+export interface AssignUserToGroupRequest {
+  /**
+   * User id associated to user to be assigned to group
+   */
+  userId: string;
+  /**
+   * Group id associated to the group the user is being assigned to
+   */
+  groupId: string;
+}
 
-export interface AssignUserToGroupRequest {}
+/**
+ * Response object for DynamicPermissionsPlugin's assignUserToGroup
+ */
+export interface AssignUserToGroupResponse {
+  /**
+   * States whether the user was successfully assigned to the group
+   */
+  assigned: boolean;
+}
+/**
+ * Request object for DynamicPermissionsPlugin's removeUserFromGroup
+ */
+export interface RemoveUserFromGroupRequest {
+  /**
+   * User id associated to the user being removed from group
+   */
+  userId: string;
+  /**
+   * Group id associated to the group the user is being removed from
+   */
+  groupId: string;
+}
+/**
+ * Response object for DynamicPermissionsPlugin's removeUserFromGroup
+ */
+export interface RemoveUserFromGroupResponse {
+  /**
+   * States whether the user was successfully removed from group
+   */
+  removed: boolean;
+}
+/**
+ * Request object for DynamicPermissionsPlugin's getIdentityPermissionsForSubject
+ */
+export interface GetIdentityPermissionsForSubjectRequest {
+  /**
+   * Subject associated to the identity permissions
+   */
+  subject: string;
 
-export interface AssignUserToGroupResponse {}
-export interface RemoveUserFromGroupRequest {}
+  /**
+   * Subject id associated to the subject
+   */
+  subjectId: string;
 
-export interface RemoveUserFromGroupResponse {}
+  /**
+   * Filter the permissions associated to the user
+   */
+  userId?: string;
+}
 
-export interface IsAuthorizedOnSubjectRequest {}
-
-export interface IsAuthorizedOnSubjectResponse {}
-
-export interface UpdateIdentityPermissionRequest {}
-
-export interface UpdateIdentityPermissionResponse {}
-
-export interface GetIdentityPermissionsForSubjectRequest {}
-
-export interface GetIdentityPermissionsForSubjectResponse {}
+/**
+ * Response object for DynamicPermissionsPlugin's getIdentityPermissionsForSubject
+ */
+export interface GetIdentityPermissionsForSubjectResponse {
+  /**
+   * an array of {@link IdentityPermission} associated to the subject
+   */
+  identityPermissions: IdentityPermission[];
+}
