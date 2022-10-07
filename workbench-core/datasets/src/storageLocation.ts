0884398c41bf5d618fa4142a2512c2647ef1981c
bhudnell <bhudnell@users.noreply.github.com>
2022-10-07 19:52:38 +0000

    docs: Auto add license headers

--- workbench-core/datasets/src/storageLocation.ts
+++ workbench-core/datasets/src/storageLocation.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 export interface StorageLocation {
   name: string;
   awsAccountId?: string;
