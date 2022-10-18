71a22fd64f55a8c4300c13432e299384ff48fd30
Tim Nguyen <thingut@amazon.com>
2022-10-18 12:39:22 -0400

    feat: pull root user email from SSM param store

--- solutions/swb-reference/src/constants.ts
+++ solutions/swb-reference/src/constants.ts
@@ -5,9 +5,10 @@
 
 import fs from 'fs';
 import { join } from 'path';
+import { AwsService } from '@aws/workbench-core-base';
 import yaml from 'js-yaml';
 
-function getConstants(): {
+interface Constants {
   STAGE: string;
   STACK_NAME: string;
   SC_PORTFOLIO_NAME: string;
@@ -21,7 +22,6 @@ function getConstants(): {
   S3_ARTIFACT_BUCKET_BOOTSTRAP_PREFIX: string;
   LAUNCH_CONSTRAINT_ROLE_OUTPUT_KEY: string;
   AMI_IDS_TO_SHARE: string;
-  ROOT_USER_EMAIL: string;
   USER_POOL_CLIENT_NAME: string;
   USER_POOL_NAME: string;
   STATUS_HANDLER_ARN_OUTPUT_KEY: string;
@@ -34,14 +34,15 @@ function getConstants(): {
   CLIENT_ID: string;
   CLIENT_SECRET: string;
   MAIN_ACCT_ENCRYPTION_KEY_ARN_OUTPUT_KEY: string;
-} {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const config: any = yaml.load(
-    // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
-    // correct file
-    // eslint-disable-next-line security/detect-non-literal-fs-filename
-    fs.readFileSync(join(__dirname, `../../src/config/${process.env.STAGE}.yaml`), 'utf8') // nosemgrep
-  );
+}
+
+interface SecretConstants {
+  ROOT_USER_EMAIL: string;
+}
+
+//CDK Constructs doesn't support Promises https://github.com/aws/aws-cdk/issues/8273
+function getConstants(): Constants {
+  const config = getConfig();
 
   const STACK_NAME = `swb-${config.stage}-${config.awsRegionShortName}`;
   const SC_PORTFOLIO_NAME = `swb-${config.stage}-${config.awsRegionShortName}`; // Service Catalog Portfolio Name
@@ -50,7 +51,6 @@ function getConstants(): {
   const S3_ACCESS_BUCKET_PREFIX = 'service-workbench-access-log';
   const S3_ARTIFACT_BUCKET_SC_PREFIX = 'service-catalog-cfn-templates/';
   const S3_ARTIFACT_BUCKET_BOOTSTRAP_PREFIX = 'environment-files/'; // Location of env bootstrap scripts in the artifacts bucket
-  const ROOT_USER_EMAIL = config.rootUserEmail;
   const allowedOrigins: string[] = config.allowedOrigins || [];
   const uiClientURL = getUiClientUrl();
   if (uiClientURL) allowedOrigins.push(uiClientURL);
@@ -87,7 +87,6 @@ function getConstants(): {
     S3_ARTIFACT_BUCKET_BOOTSTRAP_PREFIX,
     LAUNCH_CONSTRAINT_ROLE_OUTPUT_KEY,
     AMI_IDS_TO_SHARE: JSON.stringify(AMI_IDS),
-    ROOT_USER_EMAIL,
     USER_POOL_CLIENT_NAME,
     USER_POOL_NAME,
     ALLOWED_ORIGINS: JSON.stringify(allowedOrigins),
@@ -101,6 +100,34 @@ function getConstants(): {
     CLIENT_SECRET,
     MAIN_ACCT_ENCRYPTION_KEY_ARN_OUTPUT_KEY
   };
+}
+
+async function getConstantsWithSecrets(): Promise<Constants & SecretConstants> {
+  const config = getConfig();
+  const AWS_REGION = config.awsRegion;
+  const awsService = new AwsService({ region: AWS_REGION });
+  const rootUserParamStorePath = config.rootUserEmailParamStorePath;
+
+  const ROOT_USER_EMAIL = await getSSMParamValue(awsService, rootUserParamStorePath);
+  return { ...getConstants(), ROOT_USER_EMAIL };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getConfig(): any {
+  return yaml.load(
+    // __dirname is a variable that reference the current directory. We use it so we can dynamically navigate to the
+    // correct file
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    fs.readFileSync(join(__dirname, `../../src/config/${process.env.STAGE}.yaml`), 'utf8') // nosemgrep
+  );
+}
+async function getSSMParamValue(awsService: AwsService, ssmParamName: string): Promise<string> {
+  const response = await awsService.clients.ssm.getParameter({
+    Name: ssmParamName,
+    WithDecryption: true
+  });
+
+  return response.Parameter!.Value!;
 }
 
 function getUiClientUrl(): string {
@@ -124,4 +151,4 @@ function getUiClientUrl(): string {
   }
 }
 
-export { getConstants };
+export { getConstants, getConstantsWithSecrets };
