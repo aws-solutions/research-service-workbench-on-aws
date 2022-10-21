ba03afae520e4c450bd5f9c57aba2a2825c32c8b
aws-tyler <aws-tyler@users.noreply.github.com>
2022-10-21 22:18:33 +0000

    docs: Auto add license headers

--- workbench-core/accounts/src/models/account.ts
+++ workbench-core/accounts/src/models/account.ts
@@ -1,6 +1,11 @@
 /*
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
 
  */
 
