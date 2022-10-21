d9e67a90ab058775cdc513991e9f58424ab3ee3d
Sanket Dharwadkar <sdharwad@amazon.com>
2022-10-21 10:31:01 -0700

    fix: consistent error response from ALB vs APIGW (#565)
    * fix: consistent error response from ALB vs APIGW

--- solutions/swb-reference/src/proxyHandlerLambda.ts
+++ solutions/swb-reference/src/proxyHandlerLambda.ts
@@ -3,7 +3,8 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import HttpError from '../integration-tests/support/utils/HttpError';
 
 /* eslint-disable-next-line */
 export async function handler(event: any) {
@@ -48,32 +49,57 @@ export async function handler(event: any) {
   if (event.queryStringParameters) params = event.queryStringParameters;
   if (event.httpMethod) HTTP_METHOD = event.httpMethod;
 
-  if (HTTP_METHOD === 'GET') {
-    const { data, status, statusText } = await axios.get(targetPath, { headers, params });
-    response.statusCode = status;
-    response.statusDescription = statusText;
-    response.body = JSON.stringify(data);
-  }
+  try {
+    if (HTTP_METHOD === 'GET') {
+      const { data, status, statusText } = await axios.get(targetPath, { headers, params });
+      response.statusCode = status;
+      response.statusDescription = statusText;
+      response.body = JSON.stringify(data);
+    }
 
-  if (HTTP_METHOD === 'POST') {
-    const { data, status, statusText } = await axios.post(targetPath, { headers, params, body });
-    response.statusCode = status;
-    response.statusDescription = statusText;
-    response.body = JSON.stringify(data);
-  }
+    if (HTTP_METHOD === 'POST') {
+      const { data, status, statusText } = await axios.post(targetPath, body, { headers, params });
+      response.statusCode = status;
+      response.statusDescription = statusText;
+      response.body = JSON.stringify(data);
+    }
 
-  if (HTTP_METHOD === 'PUT') {
-    const { data, status, statusText } = await axios.put(targetPath, { headers, params, body });
-    response.statusCode = status;
-    response.statusDescription = statusText;
-    response.body = JSON.stringify(data);
-  }
+    if (HTTP_METHOD === 'PUT') {
+      const { data, status, statusText } = await axios.put(targetPath, body, { headers, params });
+      response.statusCode = status;
+      response.statusDescription = statusText;
+      response.body = JSON.stringify(data);
+    }
 
-  if (HTTP_METHOD === 'DELETE') {
-    const { data, status, statusText } = await axios.delete(targetPath, { headers, params });
-    response.statusCode = status;
-    response.statusDescription = statusText;
-    response.body = JSON.stringify(data);
+    if (HTTP_METHOD === 'DELETE') {
+      const { data, status, statusText } = await axios.delete(targetPath, { headers, params });
+      response.statusCode = status;
+      response.statusDescription = statusText;
+      response.body = JSON.stringify(data);
+    }
+  } catch (err) {
+    if (!(err instanceof AxiosError) || !err.response) {
+      console.error(`Unsupported error encountered: ${JSON.stringify(err)}`);
+      throw err;
+    }
+
+    const error = new HttpError(
+      err.response!.status,
+      JSON.stringify({
+        statusCode: err.response!.status,
+        error: err.response!.data.error,
+        message: err.response!.data.message
+      }),
+      err.response!.statusText
+    );
+
+    error.isBase64Encoded = false;
+    error.headers = { 'Content-Type': 'application/json' };
+
+    console.error(`Application error encountered: ${JSON.stringify(error)}`);
+
+    // The error object has to be returned like this for integration tests' backwards compatibility
+    return error;
   }
 
   // Logging what we returned for the given API call
