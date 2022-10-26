b07775ef3afb197fe48bbf5d925693003324d2d8
HaiTao Zhang <haitaozhang246@gmail.com>
2022-10-25 18:28:55 -0700

    feat: dynamic authz service implemented

--- workbench-core/authorization/src/dynamicAuthorization/dynamicAuthorizationService.ts
+++ workbench-core/authorization/src/dynamicAuthorization/dynamicAuthorizationService.ts
@@ -1,5 +1,7 @@
 import { AuthenticatedUser } from '../authenticatedUser';
+import AuthorizationPlugin from '../authorizationPlugin';
 import { DynamicOperation } from '../operation';
+import { DynamicPermissionsPlugin } from './dynamicPermissionsPlugin';
 import {
   AssignUserToGroupRequest,
   AssignUserToGroupResponse,
@@ -17,6 +19,8 @@ import {
   GetUserGroupsResponse,
   GetUsersFromGroupRequest,
   GetUsersFromGroupResponse,
+  IdentityPermission,
+  IdentityType,
   RemoveUserFromGroupRequest,
   RemoveUserFromGroupResponse
 } from './dynamicPermissionsPluginInputs';
@@ -32,6 +36,17 @@ export interface IsAuthorizedOnSubjectRequest {
 }
 
 export class DynamicAuthorizationService {
+  private _dynamicPermissisonsPlugin: DynamicPermissionsPlugin;
+  private _authorizationPlugin: AuthorizationPlugin;
+
+  public constructor(params: {
+    dynamicPermissionsPlugin: DynamicPermissionsPlugin;
+    authorizationPlugin: AuthorizationPlugin;
+  }) {
+    this._dynamicPermissisonsPlugin = params.dynamicPermissionsPlugin;
+    this._authorizationPlugin = params.authorizationPlugin;
+  }
+
   /**
    * Checks whether a {@link AuthenticatedUser} is authorized to perform {@link isAuthorizedOnSubjectRequest}
    * @param user - {@link AuthenticatedUser}
@@ -43,39 +58,75 @@ export class DynamicAuthorizationService {
     user: AuthenticatedUser,
     isAuthorizedOnSubjectRequest: IsAuthorizedOnSubjectRequest
   ): Promise<void> {
-    throw new Error('NotImplemented');
+    const { id, roles } = user;
+    const { subjectId, subjectType, action } = isAuthorizedOnSubjectRequest.operation;
+    const { groupIds } = await this._dynamicPermissisonsPlugin.getUserGroups({
+      userId: id
+    });
+    const groupIdIdentities = Array.from(new Set<string>([...groupIds, ...roles])).map(
+      (groupId): { identityType: IdentityType; identityId: string } => {
+        return {
+          identityType: 'GROUP',
+          identityId: groupId
+        };
+      }
+    );
+    const userIdentity: { identityType: IdentityType; identityId: string } = {
+      identityType: 'USER',
+      identityId: id
+    };
+    const identities: { identityType: IdentityType; identityId: string }[] = [
+      userIdentity,
+      ...groupIdIdentities
+    ];
+    //Check for subject id permissions
+    const { identityPermissions } = await this._dynamicPermissisonsPlugin.getIdentityPermissionsBySubject({
+      subjectType,
+      subjectId,
+      action,
+      identities
+    });
+    //Check for wildcard permissions
+    const wildCardPermissionsResponse = await this._dynamicPermissisonsPlugin.getIdentityPermissionsBySubject(
+      {
+        subjectType,
+        subjectId: '*',
+        action,
+        identities
+      }
+    );
+
+    const allUserPermissions: IdentityPermission[] = [
+      ...identityPermissions,
+      ...wildCardPermissionsResponse.identityPermissions
+    ];
+
+    await this._authorizationPlugin.isAuthorizedOnDynamicOperations(allUserPermissions, [
+      isAuthorizedOnSubjectRequest.operation
+    ]);
   }
   /**
    * Create an authorization group
-   * @param user - {@link AuthenticatedUser}
    * @param createGroupRequest - {@link CreateGroupRequest}
    *
    * @returns - {@link CreateGroupResponse}
    *
    * @throws - {@link GroupAlreadyExistsError} Can not create a group that already exists
    */
-  public async createGroup(
-    user: AuthenticatedUser,
-    createGroupRequest: CreateGroupRequest
-  ): Promise<CreateGroupResponse> {
-    throw new Error('NotImplemented');
+  public async createGroup(createGroupRequest: CreateGroupRequest): Promise<CreateGroupResponse> {
+    return this._dynamicPermissisonsPlugin.createGroup(createGroupRequest);
   }
   /**
    * Delete an authorization group
-   * @param user - {@link AuthenticatedUser}
    * @param deleteGroupRequest - {@link DeleteGroupRequest}
    *
    * @returns - {@link DeleteGroupResponse}
    */
-  public async deleteGroup(
-    user: AuthenticatedUser,
-    deleteGroupRequest: DeleteGroupRequest
-  ): Promise<DeleteGroupResponse> {
-    throw new Error('NotImplemented');
+  public async deleteGroup(deleteGroupRequest: DeleteGroupRequest): Promise<DeleteGroupResponse> {
+    return this._dynamicPermissisonsPlugin.deleteGroup(deleteGroupRequest);
   }
   /**
    * Create identity permissions
-   * @param user - {@link AuthenticatedUser}
    * @param createIdentityPermissionsRequest - {@link CreateIdentityPermissionsRequest}
    *
    * @returns - {@link CreateIdentityPermissionsResponse}
@@ -83,87 +134,72 @@ export class DynamicAuthorizationService {
    * @throws - {@link IdentityPermissionAlreadyExistsError} Can not create an identity permission that already exists.
    */
   public async createIdentityPermissions(
-    user: AuthenticatedUser,
     createIdentityPermissionsRequest: CreateIdentityPermissionsRequest
   ): Promise<CreateIdentityPermissionsResponse> {
-    throw new Error('NotImplemented');
+    return this._dynamicPermissisonsPlugin.createIdentityPermissions(createIdentityPermissionsRequest);
   }
   /**
    * Delete identity permissions
-   * @param user - {@link AuthenticatedUser}
    * @param deleteIdentityPermissionsRequest - {@link DeleteIdentityPermissionsRequest}
    *
    * @returns - {@link DeleteIdentityPermissionsResponse}
    */
   public async deleteIdentityPermissions(
-    user: AuthenticatedUser,
     deleteIdentityPermissionsRequest: DeleteIdentityPermissionsRequest
   ): Promise<DeleteIdentityPermissionsResponse> {
-    throw new Error('NotImplemented');
+    return this._dynamicPermissisonsPlugin.deleteIdentityPermissions(deleteIdentityPermissionsRequest);
   }
   /**
    * Delete all permissions associated to the subject
-   * @param user - {@link AuthenticatedUser}
    * @param deleteSubjectPermissionsRequest - {@link DeleteSubjectPermissionsRequest}
    *
    * @returns - {@link DeleteSubjectPermissionsResponse}
    */
   public async deleteSubjectPermissions(
-    user: AuthenticatedUser,
     deleteSubjectPermissionsRequest: DeleteSubjectPermissionsRequest
   ): Promise<DeleteSubjectPermissionsResponse> {
-    throw new Error('NotImplemented');
+    return this._dynamicPermissisonsPlugin.deleteSubjectPermissions(deleteSubjectPermissionsRequest);
   }
   /**
    * Assign a user to an authorization group
-   * @param user - {@link AuthenticatedUser}
    * @param assignUserToGroupRequest - {@link AssignUserToGroupRequest}
    *
    * @returns - {@link AssignUserToGroupResponse}
    */
   public async assignUserToGroup(
-    user: AuthenticatedUser,
     assignUserToGroupRequest: AssignUserToGroupRequest
   ): Promise<AssignUserToGroupResponse> {
-    throw new Error('NotImplemented');
+    return this._dynamicPermissisonsPlugin.assignUserToGroup(assignUserToGroupRequest);
   }
   /**
    * Remove a user from an authorization group
-   * @param user - {@link AuthenticatedUser}
    * @param removeUserFromGroupRequest - {@link RemoveUserFromGroupRequest}
    *
    * @returns - {@link RemoveUserFromGroupResponse}
    */
   public async removeUserFromGroup(
-    user: AuthenticatedUser,
     removeUserFromGroupRequest: RemoveUserFromGroupRequest
   ): Promise<RemoveUserFromGroupResponse> {
-    throw new Error('NotImplemented');
+    return await this._dynamicPermissisonsPlugin.removeUserFromGroup(removeUserFromGroupRequest);
   }
   /**
    * Get all users associated to the group
-   * @param user - {@link AuthenticatedUser}
    * @param getUsersFromGroupRequest - {@link GetUserGroupsRequest}
    *
    * @returns - {@link GetUsersFromGroupResponse}
    */
   public async getUsersFromGroup(
-    user: AuthenticatedUser,
     getUsersFromGroupRequest: GetUsersFromGroupRequest
   ): Promise<GetUsersFromGroupResponse> {
-    throw new Error('NotImplemented');
+    return this._dynamicPermissisonsPlugin.getUsersFromGroup(getUsersFromGroupRequest);
   }
   /**
    * Get all groups associated to the user
-   * @param user - {@link AuthenticatedUser}
    * @param getUserGroupsRequest - {@link GetUserGroupsRequest}
    *
    * @returns - {@link GetUserGroupsResponse}
    */
-  public async getUserGroups(
-    user: AuthenticatedUser,
-    getUserGroupsRequest: GetUserGroupsRequest
-  ): Promise<GetUserGroupsResponse> {
-    throw new Error('NotImplemented');
+  public async getUserGroups(getUserGroupsRequest: GetUserGroupsRequest): Promise<GetUserGroupsResponse> {
+    return this._dynamicPermissisonsPlugin.getUserGroups(getUserGroupsRequest);
   }
 }
