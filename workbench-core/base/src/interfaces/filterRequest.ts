89f0818531173b087cbeb0a860618dbca1264b67
Fernando Aranda <farandac@amazon.com>
2022-11-03 14:55:42 -0700

    chore(filter utils): remove unnecessary line

--- workbench-core/base/src/interfaces/filterRequest.ts
+++ workbench-core/base/src/interfaces/filterRequest.ts
@@ -6,6 +6,5 @@
 import { QueryParameterFilter } from './queryParameterFilter';
 
 export interface FilterRequest {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: QueryParameterFilter<unknown> | undefined;
 }
