6cc550eaa3b7723b8a03e091725231fa33acdc67
aws-tyler <aws-tyler@users.noreply.github.com>
2022-11-04 01:24:09 +0000

    docs: Auto add license headers

--- workbench-core/base/src/interfaces/databaseService.ts
+++ workbench-core/base/src/interfaces/databaseService.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import QueryParams from '../constants/queryParams';
 import ExecuteQueryResult from './executeQueryResult';
 
