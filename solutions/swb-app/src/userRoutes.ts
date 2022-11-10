dbc02f49603d7e8fa55d6ce825fd201696071b49
Sanket Dharwadkar <sdharwad@amazon.com>
2022-11-10 11:34:06 -0800

    feat: add permissions to lambda role

--- solutions/swb-app/src/userRoutes.ts
+++ solutions/swb-app/src/userRoutes.ts
@@ -66,25 +66,22 @@ export function setUpUserRoutes(router: Router, user: UserManagementService): vo
     '/users/:userId',
     wrapAsync(async (req: Request, res: Response) => {
       processValidatorResult(validate(req.body, UpdateUserSchema));
-
-      // Since service requires User object, uid attribute has to be added
-      req.body.uid = req.params.userId;
-
-      // Schema verifies status has to be one of these: ACTIVE|INACTIVE
-      req.body.status = req.body.status === 'ACTIVE' ? Status.ACTIVE : Status.INACTIVE;
-
       const userId = req.params.userId;
       try {
         const existingUser = await user.getUser(userId);
-        if (existingUser.status !== req.body.status) {
-          if (req.body.status === Status.ACTIVE) {
+
+        if (!_.isUndefined(req.body.status)) {
+          if (req.body.status === 'ACTIVE' && existingUser.status === Status.INACTIVE)
             await user.activateUser(userId);
-          } else if (req.body.status === Status.INACTIVE) {
+          if (req.body.status === 'INACTIVE' && existingUser.status === Status.ACTIVE)
             await user.deactivateUser(userId);
-          }
+          delete req.body.status; // Status update is complete
         }
 
-        await user.updateUser(userId, req.body);
+        // Since updateUser() requires object of type User
+        const userToUpdate = _.assign(existingUser, req.body);
+
+        await user.updateUser(userId, userToUpdate);
         res.status(204).send();
       } catch (err) {
         if (err instanceof UserNotFoundError) throw Boom.notFound(`Could not find user ${userId}`);
