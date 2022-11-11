4131786e6b525e349e668fd68ec271caf6d56e1a
Brendon Hudnell <bhudnell@amazon.com>
2022-11-11 12:06:57 -0700

    test: activate/deactiveUser integration tests (#606)
    * test: added intital create user test framework
    
    * refactor: refactored user management to use id instead of uid
    
    BREAKING CHANGE: User interface now uses `id` instead of `uid`
    
    * test: updated test and framework
    
    * docs: added rush change file
    
    * fix: added viperlight ignore
    
    * test: added activate/deactive user integ tests
    
    * test: updated snapshots
    
    * fix: pr fixes
    
    * fix: updated snapshot
    
    * fix: added more negative tests
    
    * fix: updated snapshot
    
    * revert: revert rate limiter removal
    
    * fix: replaced random generated uuid with an obviously fake one
    
    * fix: updated some files
    
    * fix: viperlight fix
    
    * fix: fixes from PR
    
    * fix: wrapped cleanup in try/catch
    
    * fix: added random uuid to emails

--- workbench-core/example/infrastructure/integration-tests/tests/users/createUser.test.ts
+++ workbench-core/example/infrastructure/integration-tests/tests/users/createUser.test.ts
@@ -4,10 +4,10 @@
  */
 
 import { CreateUser, Status } from '@aws/workbench-core-authentication';
+import { v4 as uuidv4 } from 'uuid';
 import ClientSession from '../../support/clientSession';
 import Setup from '../../support/setup';
 import HttpError from '../../support/utils/HttpError';
-import { checkHttpError } from '../../support/utils/utilities';
 
 describe('userManagement create user integration test', () => {
   const setup: Setup = new Setup();
@@ -20,7 +20,7 @@ describe('userManagement create user integration test', () => {
     user = {
       firstName: 'Test',
       lastName: 'User',
-      email: 'fakeemail@notanemail.com'
+      email: `success+create-user-${uuidv4()}@simulator.amazonses.com`
     };
   });
 
@@ -42,135 +42,61 @@ describe('userManagement create user integration test', () => {
     });
   });
 
-  it('should return an error when a user with the provided email already exists', async () => {
-    try {
-      await adminSession.resources.users.create(user);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'An account with the given email already exists.'
-        })
-      );
-    }
+  it('should return a 400 error when a user with the provided email already exists', async () => {
+    await expect(adminSession.resources.users.create(user)).rejects.toThrow(new HttpError(400, {}));
   });
 
-  it('should return an error when the provided email is not a valid email address', async () => {
-    try {
-      await adminSession.resources.users.create({ ...user, email: 'notanemail' });
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'Invalid email address format.'
-        })
-      );
-    }
+  it('should return a 400 error when the provided email is not a valid email address', async () => {
+    await expect(adminSession.resources.users.create({ ...user, email: 'notanemail' })).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the email parameter is missing', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      delete invalidParam.email;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: "requires property 'email'"
-        })
-      );
-    }
+  it('should return a 400 error when the email parameter is missing', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    delete invalidParam.email;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the email parameter is the wrong type', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      invalidParam.email = 123;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'email is not of a type(s) string'
-        })
-      );
-    }
+  it('should return a 400 error when the email parameter is the wrong type', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    invalidParam.email = 123;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the firstName parameter is missing', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      delete invalidParam.firstName;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: "requires property 'firstName'"
-        })
-      );
-    }
+  it('should return a 400 error when the firstName parameter is missing', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    delete invalidParam.firstName;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the firstName parameter is the wrong type', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      invalidParam.firstName = 123;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'firstName is not of a type(s) string'
-        })
-      );
-    }
+  it('should return a 400 error when the firstName parameter is the wrong type', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    invalidParam.firstName = 123;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the lastName parameter is missing', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      delete invalidParam.lastName;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: "requires property 'lastName'"
-        })
-      );
-    }
+  it('should return a 400 error when the lastName parameter is missing', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    delete invalidParam.lastName;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 
-  it('should return an error when the lastName parameter is the wrong type', async () => {
-    try {
-      const invalidParam: { [id: string]: unknown } = { ...user };
-      invalidParam.lastName = 123;
-      await adminSession.resources.users.create(invalidParam, false);
-    } catch (e) {
-      checkHttpError(
-        e,
-        new HttpError(400, {
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'lastName is not of a type(s) string'
-        })
-      );
-    }
+  it('should return a 400 error when the lastName parameter is the wrong type', async () => {
+    const invalidParam: { [id: string]: unknown } = { ...user };
+    invalidParam.lastName = 123;
+    await expect(adminSession.resources.users.create(invalidParam, false)).rejects.toThrow(
+      new HttpError(400, {})
+    );
   });
 });
