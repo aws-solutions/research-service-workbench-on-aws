4fe0956b8a9f9aa9814b0939cdb518af39a2a204
nguyen102 <nguyen102@users.noreply.github.com>
2022-11-15 05:14:39 +0000

    docs: Auto add license headers

--- workbench-core/accounts/src/models/costCenters/listCostCentersRequest.ts
+++ workbench-core/accounts/src/models/costCenters/listCostCentersRequest.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { QueryStringParamFilterParser } from '@aws/workbench-core-base';
 import { z } from 'zod';
 
