33e27a35ea18c0b25ea2c90d08824221824d419e
maghirardelli <maghirardelli@users.noreply.github.com>
2022-11-18 15:09:24 +0000

    docs: Auto add license headers

--- workbench-core/base/src/aws/helpers/dynamoDB/interfaces/updateUnmarshalledOutput.ts
+++ workbench-core/base/src/aws/helpers/dynamoDB/interfaces/updateUnmarshalledOutput.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ConsumedCapacity, ItemCollectionMetrics } from '@aws-sdk/client-dynamodb';
 
 export interface UpdateUnmarshalledOutput {
