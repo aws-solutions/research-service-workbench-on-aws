06f04c115fcac7f10facb88840bda8548302cd3f
sevu1 <112517151+sevu1@users.noreply.github.com>
2022-11-23 16:50:02 -0600

    chore: merge from dev (#647)
    * feat: merge datasets from swb byob (#626)
    
    * feat: merge #531 from feat/swb-byob
    
    * feat: merge #534 from feat/swb-byob
    
    * feat: merge #542 from feat/swb-byob
    
    * feat: merge #550 from feat/swb-byob
    
    * fix: updated all aws-sdk dependencies to 3.186.0
    
    * fix: merge fixes
    
    * docs: added rush change file
    
    * docs: Auto README.md updates [skip ci]
    
    * feat: cost center update/list/delete APIs and Integ Tests (#643)
    
    * feat: cost center list API (#623)
    
    * feat: Update and Delete CostCenter APIs (#632)
    
    * chore: Merge develop into feat/costCenter (#640)
    
    * test: CostCenter integ tests (#642)
    
    Co-authored-by: Tim Nguyen <thingut@amazon.com>
    
    * docs: Auto README.md updates [skip ci]
    
    * fix: cost center checkDependency (#645)
    
    * feat: add check dependency function to dataset service (#646)
    
    * feat: add check dependency function to dataset service
    
    * docs: rush change
    
    * fix: do not test internals
    
    * fix: black box testing
    
    Co-authored-by: Brendon Hudnell <bhudnell@amazon.com>
    Co-authored-by: bhudnell <bhudnell@users.noreply.github.com>
    Co-authored-by: Tim Nguyen <nguyen102@users.noreply.github.com>
    Co-authored-by: Tim Nguyen <thingut@amazon.com>

--- solutions/swb-app/src/staticRouteConfig.ts
+++ solutions/swb-app/src/staticRouteConfig.ts
@@ -21,12 +21,32 @@ export const routesMap: RoutesMap = {
         action: 'CREATE',
         subject: 'CostCenter'
       }
+    ],
+    GET: [
+      {
+        action: 'READ',
+        subject: 'CostCenter'
+      }
     ]
   },
   [`/costCenters/${resourceTypeToKey.costCenter.toLowerCase()}-${uuidRegExpAsString}`]: {
     GET: [
       {
         action: 'READ',
+        subject: 'CostCenter'
+      }
+    ],
+    PATCH: [
+      {
+        action: 'UPDATE',
+        subject: 'CostCenter'
+      }
+    ]
+  },
+  [`/costCenters/${resourceTypeToKey.costCenter.toLowerCase()}-${uuidRegExpAsString}/softDelete`]: {
+    PUT: [
+      {
+        action: 'UPDATE',
         subject: 'CostCenter'
       }
     ]
