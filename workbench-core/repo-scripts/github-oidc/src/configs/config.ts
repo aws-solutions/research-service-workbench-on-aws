9659905ca818087b6a69f4f581c0e492040dd59e
insignias <insignias@users.noreply.github.com>
2022-12-02 18:55:29 +0000

    docs: Auto add license headers

--- workbench-core/repo-scripts/github-oidc/src/configs/config.ts
+++ workbench-core/repo-scripts/github-oidc/src/configs/config.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 export const gitHubOrgRepo: Record<string, string[]> = {
   'aws-solutions': ['solution-spark-on-aws']
   //Add more "GitHub-Orgs": ["GitHub-Repo1", "GitHub-Repo2"]
--- workbench-core/repo-scripts/github-oidc/src/github-oidc-stack.ts
+++ workbench-core/repo-scripts/github-oidc/src/github-oidc-stack.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Aws, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import {
   Effect,
