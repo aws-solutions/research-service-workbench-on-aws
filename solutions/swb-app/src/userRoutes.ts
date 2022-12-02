46b2df1aa1a6a8f08cd23a36d31946bedebccf30
Sergey Vun <sevu@amazon.com>
2022-12-02 11:28:57 -0600

    feat: user management api

--- solutions/swb-app/src/userRoutes.ts
+++ solutions/swb-app/src/userRoutes.ts
@@ -7,8 +7,14 @@
 import {
   CreateRoleSchema,
   CreateUserSchema,
+  UpdateUserSchema,
+  Status,
   UpdateRoleSchema,
-  UserManagementService
+  UserManagementService,
+  isUserNotFoundError,
+  isRoleNotFoundError,
+  isInvalidParameterError,
+  isUserAlreadyExistsError
 } from '@aws/workbench-core-authentication';
 import Boom from '@hapi/boom';
 import { Request, Response, Router } from 'express';
@@ -17,21 +23,126 @@ import _ from 'lodash';
 import { wrapAsync } from './errorHandlers';
 import { processValidatorResult } from './validatorHelper';
 
-export function setUpUserRoutes(router: Router, user: UserManagementService): void {
+export function setUpUserRoutes(router: Router, userService: UserManagementService): void {
   router.post(
     '/users',
     wrapAsync(async (req: Request, res: Response) => {
-      processValidatorResult(validate(req.body, CreateUserSchema));
-      const response = await user.createUser(req.body);
-      res.status(201).send(response);
+      try {
+        processValidatorResult(validate(req.body, CreateUserSchema));
+        const response = await userService.createUser(req.body);
+        res.status(201).send(response);
+      } catch (err) {
+        if (isInvalidParameterError(err)) {
+          throw Boom.badRequest(`Invalid parameter: ${err.message}`);
+        }
+
+        if (isUserAlreadyExistsError(err)) {
+          throw Boom.conflict(`User with this email already exist.`);
+        }
+
+        if (Boom.isBoom(err)) {
+          throw err;
+        }
+
+        throw Boom.badImplementation(`Could not find create user`);
+      }
     })
   );
 
   router.get(
     '/users',
     wrapAsync(async (req: Request, res: Response) => {
-      const users = await user.listUsers();
+      const users = await userService.listUsers();
       res.status(200).json({ users });
+    })
+  );
+
+  router.get(
+    '/users/:userId',
+    wrapAsync(async (req: Request, res: Response) => {
+      const userId = req.params.userId;
+      try {
+        const user = await userService.getUser(userId);
+        res.status(200).json(user);
+      } catch (err) {
+        if (isUserNotFoundError(err)) throw Boom.notFound(`Could not find user ${userId}`);
+        if (Boom.isBoom(err)) throw err;
+        throw Boom.badImplementation(`Could not find user ${userId}`);
+      }
+    })
+  );
+
+  router.delete(
+    '/users/:userId',
+    wrapAsync(async (req: Request, res: Response) => {
+      const userId = req.params.userId;
+      try {
+        const existingUser = await userService.getUser(userId);
+        if (existingUser.status !== Status.INACTIVE) {
+          throw Boom.badRequest(
+            `Could not delete user ${userId}. Expected status: ${Status[Status.INACTIVE]}; received: ${
+              Status[existingUser.status]
+            }`
+          );
+        }
+
+        await userService.deleteUser(userId);
+        res.status(204).send();
+      } catch (err) {
+        if (isUserNotFoundError(err)) throw Boom.notFound(`Could not find user ${userId}`);
+        if (Boom.isBoom(err)) throw err;
+        throw Boom.badImplementation(`Could not delete user ${userId}`);
+      }
+    })
+  );
+
+  router.patch(
+    '/users/:userId',
+    wrapAsync(async (req: Request, res: Response) => {
+      processValidatorResult(validate(req.body, UpdateUserSchema));
+      const userId = req.params.userId;
+      try {
+        const existingUser = await userService.getUser(userId);
+
+        if (!_.isUndefined(req.body.status)) {
+          if (req.body.status === 'ACTIVE' && existingUser.status === Status.INACTIVE)
+            await userService.activateUser(userId);
+          if (req.body.status === 'INACTIVE' && existingUser.status === Status.ACTIVE)
+            await userService.deactivateUser(userId);
+          delete req.body.status; // Status update is complete, and type is different than expected for further steps
+        }
+
+        if (!_.isEmpty(req.body.roles) && !_.isEqual(existingUser.roles, req.body.roles)) {
+          const rolesToAdd = _.difference(req.body.roles, existingUser.roles);
+          await Promise.all(
+            _.map(rolesToAdd, async (role) => {
+              await userService.addUserToRole(userId, role);
+            })
+          );
+          const rolesToRemove = _.difference(existingUser.roles, req.body.roles);
+          await Promise.all(
+            _.map(rolesToRemove, async (role) => {
+              await userService.removeUserFromRole(userId, role);
+            })
+          );
+        }
+
+        // Since updateUser() requires object of type User
+        await userService.updateUser(userId, { ...existingUser, ...req.body });
+        res.status(204).send();
+      } catch (err) {
+        if (isUserNotFoundError(err)) throw Boom.notFound(`Could not find user ${userId}`);
+        if (isRoleNotFoundError(err))
+          throw Boom.notFound(
+            'Please make sure all specified roles exist as groups in the Cognito User Pool'
+          );
+        if (isInvalidParameterError(err))
+          throw Boom.notFound(
+            'Please make sure specified email is in valid email format and not already in use in the Cognito User Pool'
+          );
+        if (Boom.isBoom(err)) throw err;
+        throw Boom.badImplementation(`Could not update user ${userId}`);
+      }
     })
   );
 
@@ -39,7 +150,7 @@ export function setUpUserRoutes(router: Router, user: UserManagementService): vo
     '/roles',
     wrapAsync(async (req: Request, res: Response) => {
       processValidatorResult(validate(req.body, CreateRoleSchema));
-      const response = await user.createRole(req.body.roleName);
+      const response = await userService.createRole(req.body.roleName);
       res.status(201).send(response);
     })
   );
@@ -51,7 +162,7 @@ export function setUpUserRoutes(router: Router, user: UserManagementService): vo
       if (!_.isString(req.params.roleName)) {
         throw Boom.badRequest('roleName must be a string.');
       }
-      const response = await user.addUserToRole(req.body.username, req.params.roleName);
+      const response = await userService.addUserToRole(req.body.username, req.params.roleName);
       res.send(response);
     })
   );
