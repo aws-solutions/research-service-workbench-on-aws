de6c673b1123855aafac3f541652470ca5a00e00
Samir Sharan <insignias@users.noreply.github.com>
2022-12-06 18:44:22 -0500

    chore: save work

--- workbench-core/repo-scripts/github-oidc/src/github-oidc-stack.ts
+++ workbench-core/repo-scripts/github-oidc/src/github-oidc-stack.ts
@@ -3,8 +3,15 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
-import { FederatedPrincipal, ManagedPolicy, OpenIdConnectProvider, Role } from 'aws-cdk-lib/aws-iam';
+import { Aws, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  Effect,
+  FederatedPrincipal,
+  ManagedPolicy,
+  OpenIdConnectProvider,
+  PolicyStatement,
+  Role
+} from 'aws-cdk-lib/aws-iam';
 import { NagSuppressions } from 'cdk-nag';
 import { Construct } from 'constructs';
 
@@ -32,8 +39,92 @@ export class GitHubOIDCStack extends Stack {
             }
           },
           'sts:AssumeRoleWithWebIdentity'
-        ),
-        managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')]
+        )
+      });
+
+      // eslint-disable-next-line no-new
+      new ManagedPolicy(this, `${props.gitHubOrg}-${gitHubRepo}-GitHubOIDCCustomManagedPolicy`, {
+        statements: [
+          new PolicyStatement({
+            sid: 'StsAssumeRole',
+            effect: Effect.ALLOW,
+            actions: ['sts:AssumeRole', 'iam:*Role*'],
+            resources: [`arn:${Aws.PARTITION}:iam::${Aws.ACCOUNT_ID}:role/cdk-*`]
+          }),
+          new PolicyStatement({
+            sid: 'CloudformationAccess',
+            effect: Effect.ALLOW,
+            actions: [
+              'cloudformation:DescribeStacks',
+              'cloudformation:GetTemplate',
+              'cloudformation:CreateChangeSet',
+              'cloudformation:DescribeChangeSet',
+              'cloudformation:ExecuteChangeSet',
+              'cloudformation:DescribeStackEvents',
+              'cloudformation:DeleteChangeSet'
+            ],
+            resources: [`arn:${Aws.PARTITION}:cloudformation:${Aws.REGION}:${Aws.ACCOUNT_ID}:stack/*/*`]
+          }),
+          new PolicyStatement({
+            sid: 'S3Access',
+            effect: Effect.ALLOW,
+            actions: ['s3:*Object', 's3:ListBucket', 's3:GetBucketLocation'],
+            resources: [`arn:${Aws.PARTITION}:s3:::*`]
+          }),
+          new PolicyStatement({
+            sid: 'ECRAccess',
+            effect: Effect.ALLOW,
+            actions: [
+              'ecr:SetRepositoryPolicy',
+              'ecr:GetLifecyclePolicy',
+              'ecr:PutImageScanningConfiguration',
+              'ecr:DescribeRepositories',
+              'ecr:CreateRepository',
+              'ecr:DeleteRepository',
+              'ecr:PutImageTagMutability',
+              'ecr:ListTagsForResource'
+            ],
+            resources: [`arn:${Aws.PARTITION}:ecr:${Aws.REGION}:${Aws.ACCOUNT_ID}:repository/cdk-*`]
+          }),
+          new PolicyStatement({
+            sid: 'SSMAccess',
+            effect: Effect.ALLOW,
+            actions: ['ssm:GetParameter*', 'ssm:PutParameter*', 'ssm:DeleteParameter*'],
+            resources: [`arn:${Aws.PARTITION}:ssm:${Aws.REGION}:${Aws.ACCOUNT_ID}:parameter/*`]
+          }),
+          new PolicyStatement({
+            actions: ['cognito-idp:DescribeUserPoolClient', 'cognito-idp:AdminInitiateAuth'],
+            resources: [`arn:aws:cognito-idp:${Aws.REGION}:${this.account}:userpool/*`],
+            sid: 'CognitoAccess'
+          }),
+          new PolicyStatement({
+            actions: [
+              'dynamodb:BatchGetItem',
+              'dynamodb:GetRecords',
+              'dynamodb:GetShardIterator',
+              'dynamodb:Query',
+              'dynamodb:GetItem',
+              'dynamodb:Scan',
+              'dynamodb:ConditionCheckItem',
+              'dynamodb:BatchWriteItem',
+              'dynamodb:PutItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:DeleteItem',
+              'dynamodb:DescribeTable'
+            ],
+            resources: [
+              `arn:aws:dynamodb:${Aws.REGION}:${Aws.ACCOUNT_ID}:table/*`,
+              `arn:aws:dynamodb:${Aws.REGION}:${Aws.ACCOUNT_ID}:table/*/index/*`
+            ],
+            sid: 'DynamoDBAccess'
+          }),
+          new PolicyStatement({
+            actions: ['servicecatalog:CreatePortfolioShare', 'servicecatalog:ListPortfolios'],
+            resources: [`arn:aws:servicecatalog:${Aws.REGION}:${this.account}:*/*`],
+            sid: 'ServiceCatalogAccess'
+          })
+        ],
+        roles: [githubOIDCRole]
       });
 
       // eslint-disable-next-line no-new
