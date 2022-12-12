089784007b2258d0e314cd3ff908844136cd19a7
Marianna Ghirardelli <ghirard@amazon.com>
2022-12-12 17:11:17 -0500

    chore(merge): fix merge

--- solutions/swb-app/src/staticPermissionsConfig.ts
+++ solutions/swb-app/src/staticPermissionsConfig.ts
@@ -114,36 +114,6 @@ const adminPermissions: Permission[] = [
   {
     effect: 'ALLOW',
     action: 'CREATE',
-    subject: 'CostCenter'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'UPDATE',
-    subject: 'CostCenter'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'READ',
-    subject: 'CostCenter'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'CREATE',
-    subject: 'Dataset'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'READ',
-    subject: 'Dataset'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'UPDATE',
-    subject: 'Dataset'
-  },
-  {
-    effect: 'ALLOW',
-    action: 'CREATE',
     subject: 'Environment'
   },
   {
