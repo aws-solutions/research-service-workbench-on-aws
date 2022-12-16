325cc7efc8edcaf8da03e44fa0380ed275c22a83
Tim Nguyen <thingut@amazon.com>
2022-12-16 12:08:47 -0500

    fix: pr comments

--- solutions/swb-app/src/audit/swbAuditExtractor.test.ts
+++ solutions/swb-app/src/audit/swbAuditExtractor.test.ts
@@ -10,32 +10,32 @@ import SwbAuditExtractor from './swbAuditExtractor';
 
 describe('SwbAuditExtractor', () => {
   let swbAuditExtractor: Extractor;
-  const mockMethod = 'POST';
-  const mockUrl = '/sample';
-  const mockIp = 'sampleIP';
-  const mockUser: AuthenticatedUser = {
-    id: 'sampleId',
-    roles: []
-  };
-  const mockBody = { name: 'John' };
   beforeEach(() => {
     swbAuditExtractor = new SwbAuditExtractor();
   });
   describe('getMetadata', () => {
     test('with request body', async () => {
-      const res: Response = {
-        locals: {
-          user: mockUser
-        }
-      } as unknown as Response;
-      const req: Request = {
-        headers: {
-          'x-forwarded-for': mockIp
-        },
-        method: mockMethod,
-        originalUrl: mockUrl,
-        body: mockBody
-      } as unknown as Request;
+      const mockMethod = 'POST';
+      const mockUrl = '/sample';
+      const mockIp = 'sampleIP';
+      const mockUser: AuthenticatedUser = {
+        id: 'sampleId',
+        roles: []
+      };
+      const mockBody = { name: 'John' };
+
+      const res: Response = {} as Response;
+      res.locals = {
+        user: mockUser
+      };
+      const req: Request = {} as Request;
+      req.headers = {
+        'x-forwarded-for': mockIp
+      };
+      req.method = mockMethod;
+      req.originalUrl = mockUrl;
+      req.body = mockBody;
+
       const response = swbAuditExtractor.getMetadata(req, res);
       expect(response).toEqual({
         action: `${mockMethod} ${mockUrl}`,
--- workbench-core/audit/src/auditMiddleware.ts
+++ workbench-core/audit/src/auditMiddleware.ts
@@ -49,7 +49,7 @@ export function WithAudit(
     }
     const extractor = auditConfig.extractor ?? BaseExtractor;
     const metadata: Metadata = extractor.getMetadata(req, res);
-    // TODO: Correctly propogate statusCode. StatusCode is not correct when an error is thrown. The error handler is executed after audit log is written, therefore status code is inaccurate
+    // TODO: Correctly propagate statusCode. StatusCode is not correct when an error is thrown. The error handler is executed after audit log is written, therefore status code is inaccurate
     // next();
     // metadata.statusCode = res.statusCode;
 
