6cca1b9c714d6cb33460bb1176ac7aa9da135557
Tyler Mikev <atmikev@amazon.com>
2022-12-20 13:46:29 -0600

    chore: use old table name

--- workbench-core/environments/src/services/environmentService.test.ts
+++ workbench-core/environments/src/services/environmentService.test.ts
@@ -33,7 +33,7 @@ describe('EnvironmentService', () => {
     ddbMock.reset();
   });
   const isoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
-  const tableName = 'tableName';
+  const tableName = 'exampleDDBTable';
   const dynamoDBService = new DynamoDBService({ region: process.env.AWS_REGION!, table: tableName });
   const envService = new EnvironmentService(dynamoDBService);
   const dateString = '2021-02-26T22:42:16.652Z';
