488da76da2e680c7f51ec911cb277e0186ec6fcb
FernandoAranda <FernandoAranda@users.noreply.github.com>
2022-12-22 01:48:26 +0000

    docs: Auto add license headers

--- solutions/swb-reference/src/services/projectEnvTypeConfigService.ts
+++ solutions/swb-reference/src/services/projectEnvTypeConfigService.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ProjectEnvTypeConfigPlugin, ListProjectEnvTypeConfigsRequest } from '@aws/swb-app';
 import { ProjectService } from '@aws/workbench-core-accounts';
 import {
