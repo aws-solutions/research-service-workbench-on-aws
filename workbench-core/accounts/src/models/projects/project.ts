a0aa42c712a8fa3c32af353cc688d427f0e0c385
Marianna Ghirardelli <ghirard@amazon.com>
2022-12-22 18:38:12 -0500

    fix(projects): no strict on project

--- workbench-core/accounts/src/models/projects/project.ts
+++ workbench-core/accounts/src/models/projects/project.ts
@@ -7,27 +7,25 @@ import { z } from 'zod';
 import { ProjectStatus } from '../../constants/projectStatus';
 
 // eslint-disable-next-line @rushstack/typedef-var
-export const ProjectParser = z
-  .object({
-    id: z.string(),
-    name: z.string(),
-    description: z.string(),
-    costCenterId: z.string(),
-    status: z.nativeEnum(ProjectStatus),
-    createdAt: z.string(),
-    updatedAt: z.string(),
+export const ProjectParser = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  costCenterId: z.string(),
+  status: z.nativeEnum(ProjectStatus),
+  createdAt: z.string(),
+  updatedAt: z.string(),
 
-    // Account metadata
-    awsAccountId: z.string(),
-    envMgmtRoleArn: z.string(),
-    hostingAccountHandlerRoleArn: z.string(),
-    vpcId: z.string(),
-    subnetId: z.string(),
-    environmentInstanceFiles: z.string(),
-    encryptionKeyArn: z.string(),
-    externalId: z.string(),
-    accountId: z.string()
-  })
-  .strict();
+  // Account metadata
+  awsAccountId: z.string(),
+  envMgmtRoleArn: z.string(),
+  hostingAccountHandlerRoleArn: z.string(),
+  vpcId: z.string(),
+  subnetId: z.string(),
+  environmentInstanceFiles: z.string(),
+  encryptionKeyArn: z.string(),
+  externalId: z.string(),
+  accountId: z.string()
+});
 
 export type Project = z.infer<typeof ProjectParser>;
