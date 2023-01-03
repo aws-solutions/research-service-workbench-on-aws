95ba0d3927d5c80884eea6579af04971a39f7e0f
David Madsen <dmmadse@amazon.com>
2023-01-03 07:20:19 -0800

    test(datasets): replace fake authzService with mockable instance

--- workbench-core/datasets/src/wbcDataSetsAuthorizationPlugin.test.ts
+++ workbench-core/datasets/src/wbcDataSetsAuthorizationPlugin.test.ts
@@ -3,11 +3,31 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
+jest.mock('@aws/workbench-core-audit');
+jest.mock('@aws/workbench-core-authorization');
+jest.mock('@aws/workbench-core-logging');
+jest.mock('./dataSetMetadataPlugin');
+
+import { AuditService, BaseAuditPlugin, Writer } from '@aws/workbench-core-audit';
+import {
+  DDBDynamicAuthorizationPermissionsPlugin,
+  DynamicAuthorizationService,
+  WBCGroupManagementPlugin
+} from '@aws/workbench-core-authorization';
+import { AwsService, DynamoDBService } from '@aws/workbench-core-base';
+import { CognitoUserManagementPlugin, UserManagementService } from '@aws/workbench-core-user-management';
 import { AddRemoveAccessPermissionRequest } from './models/addRemoveAccessPermissionRequest';
 import { GetAccessPermissionRequest } from './models/getAccessPermissionRequest';
 import { WbcDataSetsAuthorizationPlugin } from './wbcDataSetsAuthorizationPlugin';
 
 describe('wbcDataSetsAuthorizationPlugin tests', () => {
+  let writer: Writer;
+  let audit: AuditService;
+  let aws: AwsService;
+  let ddbService: DynamoDBService;
+  let groupManagementPlugin: WBCGroupManagementPlugin;
+  let permissionsPlugin: DDBDynamicAuthorizationPermissionsPlugin;
+  let authzService: DynamicAuthorizationService;
   let plugin: WbcDataSetsAuthorizationPlugin;
 
   const dataSetId: string = 'fake-dataset-id';
@@ -27,28 +47,36 @@ describe('wbcDataSetsAuthorizationPlugin tests', () => {
   };
 
   beforeAll(() => {
-    const mockAuthService = {
-      init: jest.fn(),
-      isAuthorizedOnRoute: jest.fn(),
-      isAuthorizedOnSubject: jest.fn(),
-      isRouteIgnored: jest.fn(),
-      isRouteProtected: jest.fn(),
-      createGroup: jest.fn(),
-      deleteGroup: jest.fn(),
-      createIdentityPermissions: jest.fn(),
-      deleteIdentityPermissions: jest.fn(),
-      getIdentityPermissionsBySubject: jest.fn(),
-      removeUserFromGroup: jest.fn(),
-      addUserToGroup: jest.fn(),
-      getUserGroups: jest.fn(),
-      getGroupUsers: jest.fn(),
-      isUserAssignedToGroup: jest.fn(),
-      doesGroupExist: jest.fn(),
-      _groupManagementPlugin: jest.fn()
+    jest.resetAllMocks();
+    writer = {
+      prepare: jest.fn(),
+      write: jest.fn()
     };
+    audit = new AuditService(new BaseAuditPlugin(writer), true);
+    aws = new AwsService({
+      region: 'us-east-1',
+      credentials: {
+        accessKeyId: 'fakeKey',
+        secretAccessKey: 'fakeSecret'
+      }
+    });
+    ddbService = new DynamoDBService({ region: 'us-east-1', table: 'fakeTable' });
+    groupManagementPlugin = new WBCGroupManagementPlugin({
+      userManagementService: new UserManagementService(new CognitoUserManagementPlugin('fakeUserPool', aws)),
+      ddbService: ddbService,
+      userGroupKeyType: 'GROUP'
+    });
+    permissionsPlugin = new DDBDynamicAuthorizationPermissionsPlugin({
+      dynamoDBService: ddbService
+    });
+    authzService = new DynamicAuthorizationService({
+      groupManagementPlugin: groupManagementPlugin,
+      dynamicAuthorizationPermissionsPlugin: permissionsPlugin,
+      auditService: audit
+    });
 
     //@ts-ignore
-    plugin = new WbcDataSetsAuthorizationPlugin(mockAuthService);
+    plugin = new WbcDataSetsAuthorizationPlugin(authzService);
   });
 
   beforeEach(() => {
