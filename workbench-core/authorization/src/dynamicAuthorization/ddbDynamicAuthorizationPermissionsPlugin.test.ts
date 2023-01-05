821d5b588aa3107d8a65100814c0f3aeeae79a52
HaiTao Zhang <haitaozhang246@gmail.com>
2023-01-05 14:58:32 -0800

    feat: getIdentityPermissionsBySubject implemented (#738)
    * feat: getIdentityPermissionsBySubject implemented
    
    * docs: add change files
    
    * fix: change test name

--- workbench-core/authorization/src/dynamicAuthorization/ddbDynamicAuthorizationPermissionsPlugin.test.ts
+++ workbench-core/authorization/src/dynamicAuthorization/ddbDynamicAuthorizationPermissionsPlugin.test.ts
@@ -15,6 +15,7 @@ import {
 import { marshall } from '@aws-sdk/util-dynamodb';
 import { AwsService, JSONValue } from '@aws/workbench-core-base';
 import { mockClient, AwsStub } from 'aws-sdk-client-mock';
+import _ from 'lodash';
 import { Action } from '../action';
 import { AuthenticatedUser } from '../authenticatedUser';
 import { Effect } from '../effect';
@@ -44,6 +45,12 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
   let sampleDescription: string;
 
   let mockIdentityPermission: IdentityPermission;
+  let mockIdentityPermissionItem: Record<string, AttributeValue>;
+  let samplePartitionKey: string;
+  let sampleSortKey: string;
+  let sampleGroupIdentity: string;
+
+  let base64PaginationToken: string;
   beforeEach(() => {
     jest.resetModules(); // Most important - it clears the cache
 
@@ -81,6 +88,23 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
       fields: sampleFields,
       description: sampleDescription
     };
+
+    samplePartitionKey = `${sampleSubjectType}|${sampleSubjectId}`;
+    sampleSortKey = `${sampleAction}|${sampleEffect}|${sampleGroupType}|${sampleGroupId}`;
+    sampleGroupIdentity = `${sampleGroupType}|${sampleGroupId}`;
+    mockIdentityPermissionItem = {
+      pk: { S: samplePartitionKey },
+      sk: { S: sampleSortKey },
+      action: { S: sampleAction },
+      effect: { S: sampleEffect },
+      subjectType: { S: sampleSubjectType },
+      subjectId: { S: sampleSubjectId },
+      identity: { S: sampleGroupIdentity },
+      conditions: { M: marshall(sampleConditions) },
+      fields: { L: [] },
+      description: { S: sampleDescription }
+    };
+    base64PaginationToken = 'eyJwayI6InNhbXBsZVN1YmplY3RUeXBlfHNhbXBsZVN1YmplY3RJZCJ9';
   });
 
   describe('isRouteIgnored', () => {
@@ -108,14 +132,148 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
   });
 
   describe('getIdentityPermissionsBySubject', () => {
-    it('throws a not implemented exception', async () => {
+    test('Get identity permissions by subject, filter on action and groupId', async () => {
+      const keyConditionExpression = '#pk = :pk AND begins_with ( #sk, :sk )';
+      const filterExpression = '#id IN ( :id0 )';
+      const attributeNames = { '#pk': 'pk', '#sk': 'sk', '#id': 'identity' };
+      const attributeValues = {
+        ':pk': { S: `${sampleSubjectType}|${sampleSubjectId}` },
+        ':sk': { S: sampleAction },
+        ':id0': { S: sampleGroupIdentity }
+      };
+      mockDDB
+        .on(QueryCommand, {
+          FilterExpression: filterExpression,
+          ExpressionAttributeNames: attributeNames,
+          ExpressionAttributeValues: attributeValues,
+          KeyConditionExpression: keyConditionExpression
+        })
+        .resolvesOnce({
+          Items: [mockIdentityPermissionItem]
+        });
+
+      const response = await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+        subjectId: sampleSubjectId,
+        subjectType: sampleSubjectType,
+        action: sampleAction,
+        identities: [{ identityId: sampleGroupId, identityType: sampleGroupType }]
+      });
+      expect(response.data.identityPermissions).toStrictEqual([mockIdentityPermission]);
+    });
+
+    test('Get identity permissions by subject, filter on action', async () => {
+      const keyConditionExpression = '#pk = :pk AND begins_with ( #sk, :sk )';
+      const attributeNames = { '#pk': 'pk', '#sk': 'sk' };
+      const attributeValues = {
+        ':pk': { S: `${sampleSubjectType}|${sampleSubjectId}` },
+        ':sk': { S: sampleAction }
+      };
+      mockDDB
+        .on(QueryCommand, {
+          ExpressionAttributeNames: attributeNames,
+          ExpressionAttributeValues: attributeValues,
+          KeyConditionExpression: keyConditionExpression
+        })
+        .resolvesOnce({
+          Items: [mockIdentityPermissionItem]
+        });
+
+      const response = await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+        subjectId: sampleSubjectId,
+        subjectType: sampleSubjectType,
+        action: sampleAction
+      });
+      expect(response.data.identityPermissions).toStrictEqual([mockIdentityPermission]);
+    });
+
+    test('Get identity permissions by subject, filter on groupId', async () => {
+      const keyConditionExpression = '#pk = :pk';
+      const filterExpression = '#id IN ( :id0 )';
+      const attributeNames = { '#pk': 'pk', '#id': 'identity' };
+      const attributeValues = {
+        ':pk': { S: `${sampleSubjectType}|${sampleSubjectId}` },
+        ':id0': { S: sampleGroupIdentity }
+      };
+      mockDDB
+        .on(QueryCommand, {
+          FilterExpression: filterExpression,
+          ExpressionAttributeNames: attributeNames,
+          ExpressionAttributeValues: attributeValues,
+          KeyConditionExpression: keyConditionExpression
+        })
+        .resolvesOnce({
+          Items: [mockIdentityPermissionItem]
+        });
+
+      const response = await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+        subjectId: sampleSubjectId,
+        subjectType: sampleSubjectType,
+        identities: [{ identityId: sampleGroupId, identityType: sampleGroupType }]
+      });
+      expect(response.data.identityPermissions).toStrictEqual([mockIdentityPermission]);
+    });
+
+    test('Get identity permissions by subject with pagination token', async () => {
+      const keyConditionExpression = '#pk = :pk';
+      const filterExpression = '#id IN ( :id0 )';
+      const attributeNames = { '#pk': 'pk', '#id': 'identity' };
+      const attributeValues = {
+        ':pk': { S: `${sampleSubjectType}|${sampleSubjectId}` },
+        ':id0': { S: sampleGroupIdentity }
+      };
+      mockDDB
+        .on(QueryCommand, {
+          FilterExpression: filterExpression,
+          ExpressionAttributeNames: attributeNames,
+          ExpressionAttributeValues: attributeValues,
+          KeyConditionExpression: keyConditionExpression
+        })
+        .resolvesOnce({
+          Items: [mockIdentityPermissionItem],
+          LastEvaluatedKey: { pk: { S: samplePartitionKey } }
+        })
+        .resolvesOnce({
+          Items: [mockIdentityPermissionItem]
+        });
+      const { data, paginationToken } =
+        await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+          subjectId: sampleSubjectId,
+          subjectType: sampleSubjectType,
+          identities: [{ identityId: sampleGroupId, identityType: sampleGroupType }]
+        });
+      expect(data.identityPermissions).toStrictEqual([mockIdentityPermission]);
+      expect(paginationToken).toStrictEqual(base64PaginationToken);
+
+      const nextResponse = await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+        subjectId: sampleSubjectId,
+        subjectType: sampleSubjectType,
+        identities: [{ identityId: sampleGroupId, identityType: sampleGroupType }]
+      });
+      expect(nextResponse.data.identityPermissions).toStrictEqual([mockIdentityPermission]);
+      expect(nextResponse.paginationToken).toBeUndefined();
+    });
+    test('Exceed identity limitation, throw ThroughputExceededError', async () => {
+      const exceededIdentities = Array(101).fill({
+        identityId: sampleGroupId,
+        identityType: sampleGroupType
+      });
       await expect(
         dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
-          authenticatedUser: mockAuthenticatedUser,
-          subjectId: '',
-          subjectType: ''
+          subjectId: sampleSubjectId,
+          subjectType: sampleSubjectType,
+          identities: exceededIdentities
         })
-      ).rejects.toThrow(Error);
+      ).rejects.toThrow(ThroughputExceededError);
+    });
+    test('Get identity permissions by subject, no items returned', async () => {
+      mockDDB.on(QueryCommand).resolvesOnce({});
+
+      const response = await dynamoDBDynamicPermissionsPlugin.getIdentityPermissionsBySubject({
+        subjectId: sampleSubjectId,
+        subjectType: sampleSubjectType,
+        identities: [{ identityId: sampleGroupId, identityType: sampleGroupType }]
+      });
+      expect(response.data.identityPermissions).toStrictEqual([]);
     });
   });
 
@@ -135,6 +293,18 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
     });
     test('Create identity permissions', async () => {
       const mockIdentityPermissions: IdentityPermission[] = [mockIdentityPermission];
+      mockDDB.on(TransactWriteItemsCommand).resolvesOnce({});
+
+      const response = await dynamoDBDynamicPermissionsPlugin.createIdentityPermissions({
+        identityPermissions: mockIdentityPermissions,
+        authenticatedUser: mockAuthenticatedUser
+      });
+      expect(response.data.identityPermissions).toBe(mockIdentityPermissions);
+    });
+    test('Create identity permissions, with no conditions, fields, and descriptions', async () => {
+      const mockIdentityPermissions: IdentityPermission[] = [
+        _.omit(mockIdentityPermission, ['fields', 'conditions', 'description'])
+      ];
       mockDDB.on(TransactWriteItemsCommand).resolvesOnce({});
 
       const response = await dynamoDBDynamicPermissionsPlugin.createIdentityPermissions({
@@ -172,30 +342,19 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
         })
       ).rejects.toThrow(ThroughputExceededError);
     });
+
+    test('Create identity permissions with unknown error thrown', async () => {
+      mockDDB.on(TransactWriteItemsCommand).rejects(new Error('Random error should be caught'));
+      await expect(
+        dynamoDBDynamicPermissionsPlugin.createIdentityPermissions({
+          identityPermissions: [mockIdentityPermission],
+          authenticatedUser: mockAuthenticatedUser
+        })
+      ).rejects.toThrow(Error);
+    });
   });
 
   describe('getIdentityPermissionsByIdentity', () => {
-    let mockIdentityPermissionItem: Record<string, AttributeValue>;
-    let samplePartitionKey: string;
-    let sampleSortKey;
-    let sampleGroupIdentity;
-    beforeEach(() => {
-      samplePartitionKey = `${sampleSubjectType}|${sampleSubjectId}`;
-      sampleSortKey = `${sampleAction}|${sampleEffect}|${sampleGroupType}|${sampleGroupId}`;
-      sampleGroupIdentity = `${sampleGroupType}|${sampleGroupId}`;
-      mockIdentityPermissionItem = {
-        pk: { S: samplePartitionKey },
-        sk: { S: sampleSortKey },
-        action: { S: sampleAction },
-        effect: { S: sampleEffect },
-        subjectType: { S: sampleSubjectType },
-        subjectId: { S: sampleSubjectId },
-        identity: { S: sampleGroupIdentity },
-        conditions: { M: marshall(sampleConditions) },
-        fields: { L: [] },
-        description: { S: sampleDescription }
-      };
-    });
     test('Get identity permissions by identity', async () => {
       mockDDB.on(QueryCommand).resolvesOnce({
         Items: [mockIdentityPermissionItem]
@@ -221,7 +380,6 @@ describe('DDB Dynamic Authorization Permissions Plugin tests', () => {
     });
 
     test('Get identity permissions by identity with pagination token', async () => {
-      const base64PaginationToken = 'eyJwayI6InNhbXBsZVN1YmplY3RUeXBlfHNhbXBsZVN1YmplY3RJZCJ9';
       mockDDB
         .on(QueryCommand, {
           IndexName: 'getIdentityPermissionsByIdentity'
