f3577d3190a2dd0e484ce8dd77478ca4fecafbb8
Marianna Ghirardelli <ghirard@amazon.com>
2023-01-05 10:48:39 -0500

    chore(merge): add more files from develop

--- solutions/swb-reference/integration-tests/support/resources.ts
+++ solutions/swb-reference/integration-tests/support/resources.ts
@@ -7,6 +7,11 @@ import Accounts from './resources/accounts/accounts';
 import CostCenters from './resources/costCenters/costCenters';
 import Datasets from './resources/datasets/datasets';
 import Environments from './resources/environments/environments';
+<<<<<<< HEAD
+=======
+import EnvironmentTypes from './resources/environmentTypes/environmentTypes';
+import Projects from './resources/projects/projects';
+>>>>>>> 9dbe526 (feat: Project-environment type configs  relationship (#718))
 import Users from './resources/users/users';
 
 function getResources(clientSession: ClientSession): Resources {
@@ -15,7 +20,13 @@ function getResources(clientSession: ClientSession): Resources {
     datasets: new Datasets(clientSession),
     accounts: new Accounts(clientSession),
     users: new Users(clientSession),
+<<<<<<< HEAD
     costCenters: new CostCenters(clientSession)
+=======
+    costCenters: new CostCenters(clientSession),
+    environmentTypes: new EnvironmentTypes(clientSession),
+    projects: new Projects(clientSession)
+>>>>>>> 9dbe526 (feat: Project-environment type configs  relationship (#718))
   };
 }
 
@@ -25,6 +36,11 @@ interface Resources {
   datasets: Datasets;
   users: Users;
   costCenters: CostCenters;
+<<<<<<< HEAD
+=======
+  environmentTypes: EnvironmentTypes;
+  projects: Projects;
+>>>>>>> 9dbe526 (feat: Project-environment type configs  relationship (#718))
 }
 
 export { getResources, Resources };
