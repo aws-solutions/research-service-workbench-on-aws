9f29c2993c99c5b9dba1aec882c1166cbab8b583
bhudnell <bhudnell@users.noreply.github.com>
2023-01-06 22:08:23 +0000

    docs: Auto add license headers

--- workbench-core/datasets/src/models/addStorageExternalEndpoint.ts
+++ workbench-core/datasets/src/models/addStorageExternalEndpoint.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { EndpointConnectionStrings } from '../dataSetsStoragePlugin';
 import { DataSetsAccessLevel } from './dataSetsAccessLevel';
 
