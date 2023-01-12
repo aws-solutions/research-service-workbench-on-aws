7f9acd9d6a950139abea0e4be97890af334685e4
David Madsen <dmmadse@amazon.com>
2023-01-12 14:15:29 -0800

    test(datasets): remove Environment provisioning with dataset test
    Remove this SWB integration test until GroupManagement is up and running

--- solutions/swb-reference/integration-tests/tests/multiStep/dataset.test.ts
+++ solutions/swb-reference/integration-tests/tests/multiStep/dataset.test.ts
@@ -2,19 +2,19 @@
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  */
-import ClientSession from '../../support/clientSession';
+// import ClientSession from '../../support/clientSession';
 import Setup from '../../support/setup';
-import RandomTextGenerator from '../../support/utils/randomTextGenerator';
-import { dsUuidRegExp } from '../../support/utils/regExpressions';
-import Settings from '../../support/utils/settings';
+// import RandomTextGenerator from '../../support/utils/randomTextGenerator';
+// import { dsUuidRegExp } from '../../support/utils/regExpressions';
+// import Settings from '../../support/utils/settings';
 
 describe('multiStep dataset integration test', () => {
   const setup: Setup = new Setup();
-  const settings: Settings = setup.getSettings();
-  let adminSession: ClientSession;
+  // const settings: Settings = setup.getSettings();
+  // let adminSession: ClientSession;
 
   beforeAll(async () => {
-    adminSession = await setup.getDefaultAdminSession();
+    // adminSession = await setup.getDefaultAdminSession();
   });
 
   afterAll(async () => {
@@ -22,65 +22,73 @@ describe('multiStep dataset integration test', () => {
   });
 
   test('Environment provisioning with dataset', async () => {
-    const randomTextGenerator = new RandomTextGenerator(settings.get('runId'));
-    const datasetName = randomTextGenerator.getFakeText('env-DS-test');
+    expect('Blocked until GroupManagement through DynamicAuthz is implemented.').toBe(
+      'Blocked until GroupManagement through DynamicAuthz is implemented.'
+    );
+    //   const randomTextGenerator = new RandomTextGenerator(settings.get('runId'));
+    //   const datasetName = randomTextGenerator.getFakeText('env-DS-test');
 
-    // Create dataset
-    const dataSetBody = {
-      storageName: settings.get('DataSetsBucketName'),
-      awsAccountId: settings.get('mainAccountId'),
-      path: datasetName, // using same name to help potential troubleshooting
-      name: datasetName,
-      region: settings.get('awsRegion'),
-      owner: `${settings.get('projectId')}#PA`,
-      type: 'internal'
-    };
+    //   // Create dataset
+    //   const dataSetBody = {
+    //     storageName: settings.get('DataSetsBucketName'),
+    //     awsAccountId: settings.get('mainAccountId'),
+    //     path: datasetName, // using same name to help potential troubleshooting
+    //     name: datasetName,
+    //     region: settings.get('awsRegion'),
+    //     owner: `${settings.get('projectId')}#PA`,
+    //     type: 'internal',
+    //     permissions: [{
+    //       identity: `${settings.get('projectId')}#Researcher`,
+    //       identityType: 'GROUP',
+    //       accessLevel: 'read-only'
+    //     }]
+    //   };
 
-    const { data: dataSet } = await adminSession.resources.datasets.create(dataSetBody, false);
-    expect(dataSet).toMatchObject({
-      id: expect.stringMatching(dsUuidRegExp)
-    });
+    //   const { data: dataSet } = await adminSession.resources.datasets.create(dataSetBody, false);
+    //   expect(dataSet).toMatchObject({
+    //     id: expect.stringMatching(dsUuidRegExp)
+    //   });
 
-    // Provision environment with dataset
-    const envBody = {
-      envTypeId: settings.get('envTypeId'),
-      envTypeConfigId: settings.get('envTypeConfigId'),
-      envType: settings.get('envType'),
-      datasetIds: [dataSet.id],
-      name: randomTextGenerator.getFakeText('dataset-name'),
-      projectId: settings.get('projectId'),
-      description: 'Temporary DataSet for integration test'
-    };
-    const { data: env } = await adminSession.resources.environments.create(envBody);
+    //   // Provision environment with dataset
+    //   const envBody = {
+    //     envTypeId: settings.get('envTypeId'),
+    //     envTypeConfigId: settings.get('envTypeConfigId'),
+    //     envType: settings.get('envType'),
+    //     datasetIds: [dataSet.id],
+    //     name: randomTextGenerator.getFakeText('dataset-name'),
+    //     projectId: settings.get('projectId'),
+    //     description: 'Temporary DataSet for integration test'
+    //   };
+    //   const { data: env } = await adminSession.resources.environments.create(envBody);
 
-    // Verify environment has access point for dataset
-    const { data: envDetails } = await adminSession.resources.environments.environment(env.id).get();
-    const awsRegion = settings.get('awsRegion');
-    const mainAccountId = settings.get('mainAccountId');
-    const accessPointName = `${dataSet.id.slice(0, 13)}-mounted-on-${env.id.slice(0, 12)}`;
-    expect(envDetails).toMatchObject({
-      ENDPOINTS: expect.arrayContaining([
-        expect.objectContaining({
-          endPointUrl: `s3://arn:aws:s3:${awsRegion}:${mainAccountId}:accesspoint/${accessPointName}`,
-          storageArn: `arn:aws:s3:::${settings.get('DataSetsBucketName')}`,
-          dataSetId: dataSet.id,
-          path: datasetName
-        })
-      ]),
-      DATASETS: expect.arrayContaining([
-        expect.objectContaining({
-          id: dataSet.id,
-          name: datasetName
-        })
-      ])
-    });
+    //   // Verify environment has access point for dataset
+    //   const { data: envDetails } = await adminSession.resources.environments.environment(env.id).get();
+    //   const awsRegion = settings.get('awsRegion');
+    //   const mainAccountId = settings.get('mainAccountId');
+    //   const accessPointName = `${dataSet.id.slice(0, 13)}-mounted-on-${env.id.slice(0, 12)}`;
+    //   expect(envDetails).toMatchObject({
+    //     ENDPOINTS: expect.arrayContaining([
+    //       expect.objectContaining({
+    //         endPointUrl: `s3://arn:aws:s3:${awsRegion}:${mainAccountId}:accesspoint/${accessPointName}`,
+    //         storageArn: `arn:aws:s3:::${settings.get('DataSetsBucketName')}`,
+    //         dataSetId: dataSet.id,
+    //         path: datasetName
+    //       })
+    //     ]),
+    //     DATASETS: expect.arrayContaining([
+    //       expect.objectContaining({
+    //         id: dataSet.id,
+    //         name: datasetName
+    //       })
+    //     ])
+    //   });
 
-    // Verify dataset has env access point listed in its external endpoints
-    const { data: dataSetDetails } = await adminSession.resources.datasets.dataset(dataSet.id).get();
-    // Dataset was created just for this test case, so we expect only one endpoint
-    expect(dataSetDetails).toMatchObject({
-      ...dataSetBody,
-      externalEndpoints: [envDetails.ENDPOINTS[0].sk.split('ENDPOINT#')[1]]
-    });
+    //   // Verify dataset has env access point listed in its external endpoints
+    //   const { data: dataSetDetails } = await adminSession.resources.datasets.dataset(dataSet.id).get();
+    //   // Dataset was created just for this test case, so we expect only one endpoint
+    //   expect(dataSetDetails).toMatchObject({
+    //     ...dataSetBody,
+    //     externalEndpoints: [envDetails.ENDPOINTS[0].sk.split('ENDPOINT#')[1]]
+    //   });
   });
 });
