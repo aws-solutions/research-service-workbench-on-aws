6426a58640ed784ce8d7dc80f2788b203c0c99b7
htztomic <htztomic@users.noreply.github.com>
2023-02-03 22:21:55 +0000

    docs: Auto add license headers

--- workbench-core/audit/src/errors/errors.test.ts
+++ workbench-core/audit/src/errors/errors.test.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { AuditIncompleteError, isAuditIncompleteError } from './auditIncompleteError';
 
 const error = new Error();
