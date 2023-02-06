676d0aa3abdf47fb111dcf66f481c08f7266678f
htztomic <htztomic@users.noreply.github.com>
2023-02-06 23:23:13 +0000

    docs: Auto add license headers

--- workbench-core/example/app/src/routes/auditRoutes.ts
+++ workbench-core/example/app/src/routes/auditRoutes.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { AuditService, Metadata, MetadataParser } from '@aws/workbench-core-audit';
 import { isAuditIncompleteError } from '@aws/workbench-core-audit/lib/errors/auditIncompleteError';
 import { DynamicAuthorizationService } from '@aws/workbench-core-authorization';
