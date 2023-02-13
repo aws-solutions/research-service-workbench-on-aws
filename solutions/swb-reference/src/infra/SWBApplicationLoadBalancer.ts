7c8c81ab9ee8f9b7ab550342a7ba8b5cadd39b0c
Sanket Dharwadkar <sdharwad@amazon.com>
2023-02-13 11:59:48 -0800

    chore: merge Solutions pipeline files (#840)
    * chore: addition of Solutions pipeline integration files

--- solutions/swb-reference/src/infra/SWBApplicationLoadBalancer.ts
+++ solutions/swb-reference/src/infra/SWBApplicationLoadBalancer.ts
@@ -3,27 +3,52 @@
  *  SPDX-License-Identifier: Apache-2.0
  */
 
+import { CfnResource } from 'aws-cdk-lib';
 import { IVpc, SubnetSelection } from 'aws-cdk-lib/aws-ec2';
 import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export interface SWBApplicationLoadBalancerProps {
   vpc: IVpc;
   subnets: SubnetSelection;
   internetFacing: boolean;
+  accessLogsBucket: Bucket;
 }
 
 export class SWBApplicationLoadBalancer extends Construct {
   public readonly applicationLoadBalancer: ApplicationLoadBalancer;
 
   public constructor(scope: Construct, id: string, props: SWBApplicationLoadBalancerProps) {
-    const { vpc, subnets, internetFacing } = props;
+    const { vpc, subnets, internetFacing, accessLogsBucket } = props;
     super(scope, id);
 
     this.applicationLoadBalancer = new ApplicationLoadBalancer(this, id, {
       vpc,
       vpcSubnets: subnets,
       internetFacing
+    });
+    this.applicationLoadBalancer.logAccessLogs(accessLogsBucket);
+
+    const albSGMetadataNode = this.applicationLoadBalancer.node.findChild('SecurityGroup').node
+      .defaultChild as CfnResource;
+    albSGMetadataNode.addMetadata('cfn_nag', {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      rules_to_suppress: [
+        {
+          id: 'W29',
+          reason: 'TODO: Security Groups found egress with port range instead of just a single port'
+        },
+        {
+          id: 'W9',
+          reason: 'TODO: Security Groups found with ingress cidr that is not /32'
+        },
+        {
+          id: 'W2',
+          reason:
+            'TODO: Security Groups found with cidr open to world on ingress.  This should never be true on instance.  Permissible on ELB.'
+        }
+      ]
     });
   }
 }
