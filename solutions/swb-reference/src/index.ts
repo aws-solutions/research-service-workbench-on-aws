2023ef0008e099c30bed885e3f1c416f352dc338
Fernando Aranda <farandac@amazon.com>
2023-02-13 09:21:02 -0800

    feat(app registry): add App registry application with backend stack in it

--- solutions/swb-reference/src/index.ts
+++ solutions/swb-reference/src/index.ts
@@ -4,10 +4,19 @@
  */
 
 /* eslint-disable no-new */
+import { WorkbenchAppRegistry } from '@aws/workbench-core-infrastructure';
 import * as cdk from 'aws-cdk-lib';
 import { SWBStack } from './SWBStack';
 
 const app: cdk.App = new cdk.App();
-new SWBStack(app);
-
+const swbBackendStack: SWBStack = new SWBStack(app);
+new WorkbenchAppRegistry(swbBackendStack, swbBackendStack.stackId, {
+  solutionId: '',
+  solutionName: '',
+  solutionVersion: '',
+  attributeGroupName: `${swbBackendStack.stackName}-AppRegistryGroup`,
+  applicationType: '',
+  appRegistryApplicationName: `${swbBackendStack.stackName}-AppRegistryApplication`
+  //destroy: true
+});
 app.synth();
