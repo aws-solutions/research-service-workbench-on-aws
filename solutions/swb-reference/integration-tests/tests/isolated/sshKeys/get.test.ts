092310e437c48a836bee81fc219b07a4c84c8518
Jane Yu <janeyu@amazon.com>
2023-02-14 16:26:49 -0800

    test(ssh api): 404 Not Found test

--- solutions/swb-reference/integration-tests/tests/isolated/sshKeys/get.test.ts
+++ solutions/swb-reference/integration-tests/tests/isolated/sshKeys/get.test.ts
@@ -7,13 +7,32 @@ import ClientSession from '../../../support/clientSession';
 import Setup from '../../../support/setup';
 import HttpError from '../../../support/utils/HttpError';
 import { checkHttpError } from '../../../support/utils/utilities';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('Get Key Pair negative tests', () => {
   const setup: Setup = new Setup();
   let adminSession: ClientSession;
+  let adminUserId: string | undefined;
+  let project: { id: string };
+
+  beforeAll(async () => {
+    adminSession = await setup.getDefaultAdminSession();
+    const { data: costCenter } = await adminSession.resources.costCenters.create({
+      name: 'test cost center',
+      accountId: setup.getSettings().get('defaultHostingAccountId'),
+      description: 'a test object'
+    });
+
+    const { data } = await adminSession.resources.projects.create({
+      name: `TestProject-${uuidv4()}`,
+      description: 'Project for list users for project tests',
+      costCenterId: costCenter.id
+    });
+
+    project = data;
+  });
 
   beforeEach(async () => {
-    adminSession = await setup.getDefaultAdminSession();
     expect.hasAssertions();
   });
 
@@ -21,19 +40,45 @@ describe('Get Key Pair negative tests', () => {
     await setup.cleanup();
   });
 
-  describe('with Project that does not exist', () => {
-    const invalidProjectId = 'proj-00000000-0000-0000-0000-000000000000';
-
-    test.skip('it throws 404 error', async () => {
+  describe('with User that is not authorized', () => {
+    let invalidUserId: string;
+    beforeEach(() => {
+      invalidUserId = '00000000-0000-0000-0000-000000000000';
+    });
+    test.skip('it throws 403 error', async () => {
       try {
-        // await adminSession.resources.projects.project(invalidProjectId).sshKeys().keyPair();
+        //TODO:
+        // get auth await adminSession.resources.projects.project(project.id).get()
+      } catch (e) {
+        // console.error(e)
+        checkHttpError(
+          e,
+          new HttpError(403, {
+            statusCode: 403,
+            error: 'User is not authorized', //User does not have access
+            message: ` ${invalidUserId} for project ${project.id}` //TODO
+          })
+        );
+      }
+    });
+  });
+
+  describe('with Project that does not exist', () => {
+    let invalidProjectId: string;
+
+    beforeEach(() => {
+      invalidProjectId = 'proj-00000000-0000-0000-0000-000000000000';
+    });
+    test('it throws 404 error', async () => {
+      try {
+        await adminSession.resources.projects.project(invalidProjectId).sshKeys().get();
       } catch (e) {
         checkHttpError(
           e,
           new HttpError(404, {
             statusCode: 404,
             error: 'Not Found',
-            message: `No key exists for user ${adminSession.getUserId()} and project ${invalidProjectId}`
+            message: `Could not find project ${invalidProjectId}`
           })
         );
       }
