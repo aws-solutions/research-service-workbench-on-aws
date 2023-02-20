3eb62234c70732fa9b16eb39e750ad664c5ec9f4
aws-tyler <aws-tyler@users.noreply.github.com>
2023-02-20 23:10:34 +0000

    docs: Auto add license headers

--- solutions/swb-app/src/dataSets/listDataSetAccessPermissionsRequestParser.ts
+++ solutions/swb-app/src/dataSets/listDataSetAccessPermissionsRequestParser.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 // eslint-disable-next-line @rushstack/typedef-var
 import { z } from 'zod';
 import { AuthenticatedUserParser } from '../users/authenticatedUser';
