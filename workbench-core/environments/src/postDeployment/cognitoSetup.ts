d1bd555a46a3245d6f0f62a6fc99dfb6b90eb7f2
David Madsen <dmmadse@amazon.com>
2023-02-23 12:08:44 -0800

    fix(cognitosetup.ts): remove obsolete Researcher group from post-deployment

--- workbench-core/environments/src/postDeployment/cognitoSetup.ts
+++ workbench-core/environments/src/postDeployment/cognitoSetup.ts
@@ -4,7 +4,6 @@
  */
 
 import {
-  GroupExistsException,
   ListUserPoolsCommandInput,
   UserPoolDescriptionType,
   UsernameExistsException
@@ -32,18 +31,6 @@ export default class CognitoSetup {
 
     const userPoolId = await this.getUserPoolId();
     console.log(`User pool id: ${userPoolId}`);
-
-    // Create Researcher groups in user pool if they do not exist
-    try {
-      await this.createGroup('Researcher', userPoolId);
-      console.log('Creating Researcher group because group does not exist');
-    } catch (e) {
-      if (e instanceof GroupExistsException) {
-        console.log(`Researcher group already exists in user pool ${USER_POOL_NAME}`);
-      } else {
-        throw e;
-      }
-    }
 
     // Create root user in user pool if user does not exist in pool
     try {
