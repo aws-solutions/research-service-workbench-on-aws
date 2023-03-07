2456677ab7b1885ea0acd14d7fe58f3e37310bca
otlci <119629557+otlci@users.noreply.github.com>
2023-03-07 08:11:59 -0800

    feat(workbenchcognito.ts): adding support for user pool advanced security mode (#928)

--- workbench-core/infrastructure/src/workbenchCognito.ts
+++ workbench-core/infrastructure/src/workbenchCognito.ts
@@ -6,6 +6,7 @@
 import { Duration, RemovalPolicy, SecretValue, Stack } from 'aws-cdk-lib';
 import {
   AccountRecovery,
+  AdvancedSecurityMode,
   Mfa,
   OAuthScope,
   UserPool,
@@ -45,7 +46,8 @@ const userPoolDefaults: UserPoolProps = {
   mfaSecondFactor: {
     sms: false,
     otp: true
-  }
+  },
+  advancedSecurityMode: AdvancedSecurityMode.ENFORCED
 };
 
 const userPoolClientDefaults: UserPoolClientOptions = {
@@ -80,6 +82,7 @@ export interface WorkbenchCognitoProps {
   refreshTokenValidity?: Duration;
   mfa?: Mfa;
   removalPolicy?: RemovalPolicy;
+  advancedSecurityMode?: AdvancedSecurityMode;
 }
 
 export interface WorkbenchUserPoolOidcIdentityProvider
@@ -107,7 +110,8 @@ export class WorkbenchCognito extends Construct {
     const tempUserPoolProps: UserPoolProps = {
       mfa: props.mfa,
       userPoolName: props.userPoolName,
-      removalPolicy: props.removalPolicy
+      removalPolicy: props.removalPolicy,
+      advancedSecurityMode: props.advancedSecurityMode
     };
 
     const userPoolProps = merge(userPoolDefaults, tempUserPoolProps);
