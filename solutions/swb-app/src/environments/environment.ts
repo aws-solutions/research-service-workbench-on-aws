2f5fc97d5fdbe7721e68c3ef783d86a379767e90
FernandoAranda <FernandoAranda@users.noreply.github.com>
2023-04-02 21:31:44 +0000

    docs: Auto add license headers

--- solutions/swb-app/src/environments/environment.ts
+++ solutions/swb-app/src/environments/environment.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { EnvironmentStatus } from '@aws/workbench-core-environments';
 
 export interface Environment {
