9bac8f79a1da7ebda22ba7fc0665d8541853c1cf
insignias <insignias@users.noreply.github.com>
2023-04-05 06:04:01 +0000

    docs: Auto add license headers

--- workbench-core/repo-scripts/github-oidc/src/constructs/swb-github-oidc-role-policy.ts
+++ workbench-core/repo-scripts/github-oidc/src/constructs/swb-github-oidc-role-policy.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Aws } from 'aws-cdk-lib';
 import { Effect, IRole, ManagedPolicy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
