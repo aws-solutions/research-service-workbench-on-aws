74f4760dd2d8c5a44a6617dcbeb362107f06d4f1
HaiTao Zhang <haitaozhang246@gmail.com>
2023-04-18 01:43:38 -0700

    fix: update shrinkwrap

--- workbench-core/authorization/src/dynamicAuthorization/models/validateUserGroups.ts
+++ workbench-core/authorization/src/dynamicAuthorization/models/validateUserGroups.ts
@@ -2,14 +2,23 @@ import { z } from 'zod';
 
 // eslint-disable-next-line @rushstack/typedef-var
 export const ValidateUserGroupsRequestParser = z.object({
+  /**
+   * User ID being validated
+   */
   userId: z.string(),
+  /**
+   * Array of group IDs being validated
+   */
   groupIds: z.array(z.string())
 });
 
+/**
+ * Request object for ValidateUserGroups
+ */
 export type ValidateUserGroupsRequest = z.infer<typeof ValidateUserGroupsRequestParser>;
 
 /**
- *
+ * Response object for ValidateUserGroups
  */
 export interface ValidateUserGroupsResponse {
   validGroupIds: string[];
