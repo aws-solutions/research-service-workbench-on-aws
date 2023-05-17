e1b22af1f07e2d1f82af79af35f3d0820e06d6a7
htztomic <htztomic@users.noreply.github.com>
2023-05-17 01:23:50 +0000

    docs: Auto add license headers

--- workbench-core/authentication/src/tokenRevocationService.ts
+++ workbench-core/authentication/src/tokenRevocationService.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { DynamoDBService, buildDynamoDBPkSk } from '@aws/workbench-core-base';
 import jwtDecode from 'jwt-decode';
 import _ from 'lodash';
