ca2a028b47d194cbc430d635a0662799a4f35ff3
jane yu <118856243+janeyuaws@users.noreply.github.com>
2023-05-19 10:57:09 -0700

    chore(viperlight): replace with authorized creds (#1080)
    * chore(viperlight): replace with authorized creds, replace email that not test against cognito

--- workbench-core/infrastructure/src/workbenchAppRegistry.test.ts
+++ workbench-core/infrastructure/src/workbenchAppRegistry.test.ts
@@ -76,7 +76,7 @@ describe('WorkbenchAppRegistry Test', () => {
       attributeGroupName: 'TestApp-Metadata',
       applicationType: 'Test',
       appRegistryApplicationName: 'TestApp',
-      accountIds: ['012345678901']
+      accountIds: ['111122223333']
     });
 
     const template = Template.fromStack(stack);
@@ -88,7 +88,7 @@ describe('WorkbenchAppRegistry Test', () => {
       PermissionArns: [
         'arn:aws:ram::aws:permission/AWSRAMPermissionServiceCatalogAppRegistryAttributeGroupAllowAssociation'
       ],
-      Principals: ['012345678901'],
+      Principals: ['111122223333'],
       ResourceArns: [
         {
           'Fn::GetAtt': ['TestStackAttributeGroup7F76BDF8', 'Arn']
@@ -106,7 +106,7 @@ describe('WorkbenchAppRegistry Test', () => {
       attributeGroupName: 'TestApp-Metadata',
       applicationType: 'Test',
       appRegistryApplicationName: 'TestApp',
-      accountIds: ['012345678901'],
+      accountIds: ['111122223333'],
       destroy: true
     });
 
@@ -133,7 +133,7 @@ describe('WorkbenchAppRegistry Test', () => {
       attributeGroupName: 'TestApp-Metadata',
       applicationType: 'Test',
       appRegistryApplicationName: 'TestApp',
-      accountIds: ['012345678901'],
+      accountIds: ['111122223333'],
       appInsights: true
     });
 
@@ -158,7 +158,7 @@ describe('WorkbenchAppRegistry Test', () => {
       attributeGroupName: 'TestApp-Metadata',
       applicationType: 'Test',
       appRegistryApplicationName: 'TestApp',
-      accountIds: ['012345678901']
+      accountIds: ['111122223333']
     });
 
     testAppRegistry.applyAppRegistryToStacks([testStack2], false);
