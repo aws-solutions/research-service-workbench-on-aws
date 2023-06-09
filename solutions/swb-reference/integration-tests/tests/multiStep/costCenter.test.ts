2ea0ea857a9cbf403f0cd077d7a92b94950be929
Tim Nguyen <thingut@amazon.com>
2023-06-08 21:40:12 -0400

    test: added negative AuthZ tests for Get and List cost center

--- solutions/swb-reference/integration-tests/tests/multiStep/costCenter.test.ts
+++ solutions/swb-reference/integration-tests/tests/multiStep/costCenter.test.ts
@@ -1,17 +1,59 @@
 import ClientSession from '../../support/clientSession';
 import Setup from '../../support/setup';
+import { PaabHelper } from '../../support/complex/paabHelper';
+import { checkHttpError } from '../../support/utils/utilities';
+import HttpError from '../../support/utils/HttpError';
 
 describe('multiStep costCenter test', () => {
   const setup: Setup = Setup.getSetup();
+  const paabHelper = new PaabHelper();
   let adminSession: ClientSession;
+  let pa1Session: ClientSession;
+  let rs1Session: ClientSession;
 
   beforeAll(async () => {
     adminSession = await setup.getDefaultAdminSession();
+    ({ pa1Session, rs1Session } = await paabHelper.createResources());
   });
 
   afterAll(async () => {
     await setup.cleanup();
   });
+
+  async function checkThatRoleCannotAccessCostCenter(
+    role: 'researcher' | 'projectAdmin',
+    costCenterId: string
+  ) {
+    console.log(`Check ${role} cannot access cost center`);
+    let session: ClientSession;
+    if (role === 'researcher') {
+      session = rs1Session;
+    } else if (role === 'projectAdmin') {
+      session = pa1Session;
+    } else {
+      throw new Error(`Role ${role} is not supported for this test function`);
+    }
+    try {
+      await session.resources.costCenters.costCenter(costCenterId).get();
+    } catch (e) {
+      checkHttpError(
+        e,
+        new HttpError(403, {
+          error: 'User is not authorized'
+        })
+      );
+    }
+    try {
+      await session.resources.costCenters.get();
+    } catch (e) {
+      checkHttpError(
+        e,
+        new HttpError(403, {
+          error: 'User is not authorized'
+        })
+      );
+    }
+  }
 
   test('create, get, update, list, delete', async () => {
     console.log('Creating Cost Centers');
@@ -20,6 +62,9 @@ describe('multiStep costCenter test', () => {
       accountId,
       name: 'costCenterA'
     });
+
+    await checkThatRoleCannotAccessCostCenter('researcher', createdCostCenterA);
+    await checkThatRoleCannotAccessCostCenter('projectAdmin', createdCostCenterA);
 
     const { data: createdCostCenterB } = await adminSession.resources.costCenters.create({
       accountId,
