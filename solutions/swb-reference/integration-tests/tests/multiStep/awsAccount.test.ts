afa27e3065395798e39271300f4009c0897608d5
Jane Yu <janeyu@amazon.com>
2023-06-22 10:30:58 -0700

    chore: store param in ssm

--- solutions/swb-reference/integration-tests/tests/multiStep/awsAccount.test.ts
+++ solutions/swb-reference/integration-tests/tests/multiStep/awsAccount.test.ts
@@ -5,6 +5,7 @@
 
 import { CreateAccountRequest } from '@aws/workbench-core-accounts';
 import { Account } from '@aws/workbench-core-accounts/lib/models/accounts/account';
+import { AwsService, SecretsService } from '@aws/workbench-core-base';
 import ClientSession from '../../support/clientSession';
 import { AccountHelper } from '../../support/complex/accountHelper';
 import Setup from '../../support/setup';
@@ -14,10 +15,15 @@ import Settings from '../../support/utils/settings';
 describe('multiStep awsAccount integration test', () => {
   const setup: Setup = Setup.getSetup();
   const settings: Settings = setup.getSettings();
+  const awsRegion = settings.get('awsRegion');
+  const externalIdPath = settings.get('externalIdPath');
+  const secretsService = new SecretsService(new AwsService({ region: awsRegion }).clients.ssm);
+  let externalId: string;
   let adminSession: ClientSession;
 
   beforeAll(async () => {
     adminSession = await setup.getDefaultAdminSession();
+    externalId = await secretsService.getSecret(externalIdPath);
   });
 
   afterAll(async () => {
@@ -31,13 +37,12 @@ describe('multiStep awsAccount integration test', () => {
       const randomTextGenerator = new RandomTextGenerator(setup.getSettings().get('runId'));
 
       const awsAccountIdToUse = settings.get('awsAccountId');
-
       const createAccountParams: CreateAccountRequest = {
         hostingAccountHandlerRoleArn: `arn:aws:iam::${awsAccountIdToUse}:role/${stackName}-hosting-account-role`,
         awsAccountId: awsAccountIdToUse,
         envMgmtRoleArn: `arn:aws:iam::${awsAccountIdToUse}:role/${stackName}-env-mgmt`,
         name: randomTextGenerator.getFakeText('fakeName'),
-        externalId: settings.get('externalId')
+        externalId: externalId
       };
 
       const createResponse = await adminSession.resources.accounts.create(createAccountParams, false);
