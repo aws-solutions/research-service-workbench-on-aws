5694ce4f00a447c7f4b091aae6a1e279a49bac39
Kevin Park <kevpark@amazon.com>
2023-06-27 14:24:46 -0700

    chore: merge main into develop

--- solutions/swb-reference/scripts/solutions-implementation/postSolutionDeployment.sh
+++ solutions/swb-reference/scripts/solutions-implementation/postSolutionDeployment.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
 read -p "Please enter your email address: " EMAIL
-export regionShortName=test
-export region=$(aws cloudformation describe-stacks --stack-name rsw-dev-test --output text --query 'Stacks[0].Outputs[?OutputKey==`awsRegion`].OutputValue')
-export cognitoUserPoolId=$(aws cloudformation describe-stacks --stack-name rsw-dev-test --output text --query 'Stacks[0].Outputs[?OutputKey==`cognitoUserPoolId`].OutputValue')
-export cognitoProgrammaticAccessUserPoolClientId=$(aws cloudformation describe-stacks --stack-name rsw-dev-test --output text --query 'Stacks[0].Outputs[?OutputKey==`cognitoProgrammaticAccessUserPoolClientId`].OutputValue')
-export dynamicAuthDDBTableName=$(aws cloudformation describe-stacks --stack-name rsw-dev-test --output text --query 'Stacks[0].Outputs[?OutputKey==`dynamicAuthDDBTableName`].OutputValue')
-aws ssm put-parameter --name "/swb/dev/rootUser/email/$regionShortName" --value $EMAIL --type 'SecureString' > /dev/null
+export region=$(aws cloudformation describe-stacks --stack-name rsw-prod-release --output text --query 'Stacks[0].Outputs[?OutputKey==`awsRegion`].OutputValue')
+export cognitoUserPoolId=$(aws cloudformation describe-stacks --stack-name rsw-prod-release --output text --query 'Stacks[0].Outputs[?OutputKey==`cognitoUserPoolId`].OutputValue')
+export cognitoProgrammaticAccessUserPoolClientId=$(aws cloudformation describe-stacks --stack-name rsw-prod-release --output text --query 'Stacks[0].Outputs[?OutputKey==`cognitoProgrammaticAccessUserPoolClientId`].OutputValue')
+export dynamicAuthDDBTableName=$(aws cloudformation describe-stacks --stack-name rsw-prod-release --output text --query 'Stacks[0].Outputs[?OutputKey==`dynamicAuthDDBTableName`].OutputValue')
+aws ssm put-parameter --name "/swb/prod/rootUser/email/$regionShortName" --value $EMAIL --type 'SecureString' > /dev/null
 
-rm -rf ./solutions/swb-reference/src/config/dev.yaml
-rm -rf ./solutions/swb-reference/src/config/dev.json
+rm -rf ./solutions/swb-reference/src/config/prod.yaml
+rm -rf ./solutions/swb-reference/src/config/prod.json
 
 echo "
-stage: dev
+stage: prod
 awsRegion: $region
 awsRegionShortName: $regionShortName
-rootUserEmailParamStorePath: '/swb/dev/rootUser/email/$regionShortName'
+rootUserEmailParamStorePath: '/swb/prod/rootUser/email/$regionShortName'
 userPoolId: $cognitoUserPoolId
-" >> ./solutions/swb-reference/src/config/dev.yaml
+" >> ./solutions/swb-reference/src/config/prod.yaml
 
 echo "
-{\"rsw-dev-test\": 
+{\"rsw-prod-release\":
    {\"dynamicAuthDDBTableName\": \"$dynamicAuthDDBTableName\",
    \"cognitoProgrammaticAccessUserPoolClientId\": \"$cognitoProgrammaticAccessUserPoolClientId\",
     \"cognitoUserPoolId\": \"$cognitoUserPoolId\",
     \"awsRegion\": \"$region\"
    }
-}" >> ./solutions/swb-reference/src/config/dev.json
+}" >> ./solutions/swb-reference/src/config/prod.json
 
 npm install -g @microsoft/rush
 rush update
 cd solutions/swb-reference
-STAGE=dev rushx run-postDeployment
-STAGE=dev rushx run-postDeployment  # Need to run this twice currently
+STAGE=prod rushx run-postDeployment
+STAGE=prod rushx run-postDeployment  # Need to run this twice currently
